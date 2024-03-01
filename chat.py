6159aae80cf7b82915ebbb2b3a249c6ebf7632a8
EricLBuehler <ericlbuehler@gmail.com>
2024-03-01 14:24:40 -0500

    Update the chat script

--- chat.py
+++ chat.py
@@ -5,7 +5,7 @@ openai.api_key = "EMPTY"
 openai.base_url = "http://localhost:1234/v1/"
 
 messages = []
-prompt = input("Enter system prompt >>>")
+prompt = input("Enter system prompt >>> ")
 messages.append({"role":"system", "content":prompt})
 
 while True:
