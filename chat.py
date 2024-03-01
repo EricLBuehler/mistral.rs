2293cae36451b59add9cf6f63ee951acd616a909
EricLBuehler <ericlbuehler@gmail.com>
2024-03-01 14:24:02 -0500

    Update the chat script

--- chat.py
+++ chat.py
@@ -5,8 +5,11 @@ openai.api_key = "EMPTY"
 openai.base_url = "http://localhost:1234/v1/"
 
 messages = []
+prompt = input("Enter system prompt >>>")
+messages.append({"role":"system", "content":prompt})
+
 while True:
-    prompt = input(">>>")
+    prompt = input(">>> ")
     messages.append({"role":"user", "content":prompt})
     completion = openai.chat.completions.create(
         model="mistral",
@@ -14,5 +17,5 @@ while True:
         max_tokens = 256,
     )
     resp = completion.choices[0].message.content
-    print(resp)
+    print(resp["[/INST]"][-1])
     messages.append({"role":"assistant", "content":resp})
