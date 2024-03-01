b518b90095103734d79c3d8f1419cacc5a68142c
EricLBuehler <65165915+EricLBuehler@users.noreply.github.com>
2024-03-01 17:59:09 +0000

    Deploying to gh-pages from @ EricLBuehler/mistral.rs@52af92a1e1cefd718d7321f81d697e34f83bc59f 🚀

--- mistralrs/sidebar-items.js
+++ mistralrs/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["chatcompletions","get_router","main"],"mod":["openai"],"struct":["Args"]};
+window.SIDEBAR_ITEMS = {"enum":["ModelSelected"],"fn":["chatcompletions","get_router","main"],"mod":["openai"],"struct":["Args"]};
---
+++ trait.impl/clap_builder/derive/trait.Subcommand.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"mistralrs":[["impl Subcommand for <a class=\"enum\" href=\"mistralrs/enum.ModelSelected.html\" title=\"enum mistralrs::ModelSelected\">ModelSelected</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
