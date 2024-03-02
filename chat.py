ad9e707495142fef11aae7f3d8277e74cda2b0ac
EricLBuehler <ericlbuehler@gmail.com>
2024-03-02 12:01:39 -0500

    Formatting

--- chat.py
+++ chat.py
@@ -6,15 +6,15 @@ openai.base_url = "http://localhost:1234/v1/"
 
 messages = []
 prompt = input("Enter system prompt >>> ")
-messages.append({"role":"system", "content":prompt})
+messages.append({"role": "system", "content": prompt})
 
 while True:
     prompt = input(">>> ")
-    messages.append({"role":"user", "content":prompt})
+    messages.append({"role": "user", "content": prompt})
     completion = openai.chat.completions.create(
         model="mistral",
         messages=messages,
-        max_tokens = 256,
+        max_tokens=256,
     )
     resp = completion.choices[0].message.content
     out = resp.split("[/INST]")[-1].strip()
@@ -22,5 +22,5 @@ while True:
         out = out[:-4]
         print(out)
     else:
-        print(out+"...")
-    messages.append({"role":"assistant", "content":out})
+        print(out + "...")
+    messages.append({"role": "assistant", "content": out})
