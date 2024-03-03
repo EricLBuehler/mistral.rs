23fff0f42d46c083d9f3f0533fae252c20b9f763
EricLBuehler <ericlbuehler@gmail.com>
2024-03-03 06:11:47 -0500

    Apply norm in normal fwd

--- mistralrs-lora/src/loralinear.rs
+++ mistralrs-lora/src/loralinear.rs
@@ -49,6 +49,7 @@ impl LoraLinear {
         let a_vb = vb.pp("lora_A".to_string());
         let b_vb = vb.pp("lora_B".to_string());
         for (name, cfg) in config.iter() {
+            println!("{name:?}");
             let a_pp = a_vb.pp(name);
             assert!(a_pp.contains_tensor("weight"));
             let a = a_pp.get_with_hints(
