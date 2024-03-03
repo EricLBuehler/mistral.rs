241acc8e527df9be9eb05b27d74c283e84b4f143
EricLBuehler <ericlbuehler@gmail.com>
2024-03-03 04:57:19 -0500

    Show adapters scalings differently

--- mistralrs-lora/src/loralinear.rs
+++ mistralrs-lora/src/loralinear.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashMap, iter::zip, ops::Mul};
 
-use candle_core::{Module, Result, Shape, Tensor};
+use candle_core::{IndexOp, Module, Result, Shape, Tensor};
 use candle_nn::{init, Dropout, Linear, VarBuilder};
 
 use crate::{
@@ -96,8 +96,7 @@ impl LinearLayerLike for LoraLinear {
     }
     fn lora_forward(&self, input: &Tensor, scalings: Tensor) -> Result<Tensor> {
         let scalings = get_maybe_topk_scalings(scalings, self.layer_n)?;
-
-        //  println!("layer scalings {scalings:?}");
+        println!("layer scalings {scalings:?} {} {} {} {} {} {} {} {} {}",scalings.i((0,0,0))?,scalings.i((0,0,1))?,scalings.i((0,0,2))?,scalings.i((0,0,3))?,scalings.i((0,0,4))?,scalings.i((0,0,5))?,scalings.i((0,0,6))?,scalings.i((0,0,7))?,scalings.i((0,0,8))?);
         
         //No fan_in_fan_out so no weight.transpose(0,1)
         let mut result = self.old.forward(input)?;
