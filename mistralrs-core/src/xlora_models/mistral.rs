1bcc22ea4a690fdc5d3fa4e5db4d97a220eaa294
EricLBuehler <ericlbuehler@gmail.com>
2024-03-03 05:25:45 -0500

    Debug

--- mistralrs-core/src/xlora_models/mistral.rs
+++ mistralrs-core/src/xlora_models/mistral.rs
@@ -276,6 +276,7 @@ impl Attention {
         let (key_states, value_states) = match &*kv_cache {
             None => (key_states, value_states),
             Some((prev_k, prev_v)) => {
+                panic!("nope");
                 let key_states = Tensor::cat(&[prev_k, &key_states], 2)?;
                 let value_states = Tensor::cat(&[prev_v, &value_states], 2)?;
                 (key_states, value_states)
