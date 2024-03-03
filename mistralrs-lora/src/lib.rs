fbfd8dc659a2a2b8063845e7446532cc1c18e5e1
EricLBuehler <ericlbuehler@gmail.com>
2024-03-02 19:30:25 -0500

    Debug print

--- mistralrs-lora/src/lib.rs
+++ mistralrs-lora/src/lib.rs
@@ -25,7 +25,7 @@ pub struct LoraConfig {
 
 fn apply_scalings_to_x(x: Tensor, scalings_layer: &Tensor, adapter: usize) -> Result<Tensor> {
     let scalings = scalings_layer.i((.., .., adapter))?.unsqueeze(D::Minus1)?;
-    println!("{:?}", scalings.to_vec2::<f32>().unwrap());
+    println!("{:?}", scalings.to_vec3::<f32>().unwrap());
     x.broadcast_mul(&scalings)
 }
 
