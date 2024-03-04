0de685fa34f182633082491aac79478f05dc4605
EricLBuehler <ericlbuehler@gmail.com>
2024-03-04 12:57:56 -0500

    Print adapter 9

--- mistralrs-lora/src/lib.rs
+++ mistralrs-lora/src/lib.rs
@@ -19,8 +19,11 @@ pub struct LoraConfig {
     target_modules: HashSet<String>,
 }
 
-fn apply_scalings_to_x(x: Tensor, scalings_layer: &Tensor, adapter: usize) -> Result<Tensor> {
+fn apply_scalings_to_x(x: Tensor, scalings_layer: &Tensor, adapter: usize, layer: usize) -> Result<Tensor> {
     let scalings = scalings_layer.i((.., .., adapter))?.unsqueeze(D::Minus1)?;
+    if layer==60 && adapter==8 {
+        dbg!(scalings.to_vec3::<half::bf16>().unwrap()[0][0][0]);
+    }
     let res = x.broadcast_mul(&scalings)?;
     Ok(res)
 }
