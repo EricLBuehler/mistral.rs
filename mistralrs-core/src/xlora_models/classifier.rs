cb9f1e3e9fb2fefccebe4e58f0a5e3479b17b0dd
EricLBuehler <ericlbuehler@gmail.com>
2024-03-04 19:09:55 -0500

    Debug by setting all to 0,1

--- mistralrs-core/src/xlora_models/classifier.rs
+++ mistralrs-core/src/xlora_models/classifier.rs
@@ -163,7 +163,7 @@ impl XLoraClassifier {
                 0..scalings.dims()[0],
                 0..scalings.dims()[1],
                 0..scalings.dims()[2],
-                self.n_classes - 2..self.n_classes-1,
+                self.n_classes - 1..self.n_classes,
             ],
             &Tensor::ones(
                 (
