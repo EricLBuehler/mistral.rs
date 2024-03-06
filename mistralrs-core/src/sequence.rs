46f2def592848a62394364a180f52821f522b900
EricLBuehler <ericlbuehler@gmail.com>
2024-03-05 20:35:09 -0500

    Debug

--- mistralrs-core/src/sequence.rs
+++ mistralrs-core/src/sequence.rs
@@ -62,6 +62,7 @@ impl Sequence {
         is_xlora: bool,
     ) -> Self {
         let prompt_len = tokens.len();
+        println!("CREATING with Id {id}");
         Self {
             tokens,
             logprobs: Vec::new(),
