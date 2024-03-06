8add944b1dd92e3a1fe5a67f9e2220e832335865
EricLBuehler <ericlbuehler@gmail.com>
2024-03-05 19:29:37 -0500

    Debug

--- mistralrs-core/src/engine/mod.rs
+++ mistralrs-core/src/engine/mod.rs
@@ -260,13 +260,14 @@ impl Engine {
             debug_assert_eq!(v_caches.len(), seqs.len());
 
             for (seq_i, seq) in seqs.iter().enumerate() {
-                if !deref_refcell!(seq).is_running() {
-                    continue;
-                }
-                
+
                 let mut seq = deref_mut_refcell!(seq);
                 let seq_cache = seq.cache();
                 let seq_cache = seq_cache.get_mut(layer).unwrap();
+                if !deref_refcell!(seq).is_running() {
+                    *seq_cache = None;
+                    continue;
+                }
                 *seq_cache = Some((
                     k_caches.get(seq_i).unwrap().clone(),
                     v_caches.get(seq_i).unwrap().clone(),
