5c8dbbc826e30601aa64a9965249a292a84c4328
EricLBuehler <65165915+EricLBuehler@users.noreply.github.com>
2024-03-07 20:04:55 +0000

    Deploying to gh-pages from @ EricLBuehler/mistral.rs@88bd88227d0f969b89c1fdf2ddac9dbb4c3a855b 🚀

--- src-files.js
+++ src-files.js
@@ -1,7 +1,7 @@
 var srcIndex = new Map(JSON.parse('[\
 ["mistralrs",["",[],["lib.rs"]]],\
-["mistralrs_core",["",[["engine",[],["mod.rs"]],["models",[],["gemma.rs","llama.rs","mistral.rs","mixtral.rs","mod.rs","quantized_llama.rs"]],["pipeline",[],["gemma.rs","llama.rs","mistral.rs","mixtral.rs","mod.rs"]],["utils",[],["mod.rs","tokens.rs","varbuilder_utils.rs"]],["xlora_models",[],["classifier.rs","config.rs","gemma.rs","llama.rs","mistral.rs","mixtral.rs","mod.rs"]]],["lib.rs","request.rs","response.rs","sampling.rs","scheduler.rs","sequence.rs"]]],\
-["mistralrs_lora",["",[],["frozenlinear.rs","lib.rs","loralinear.rs"]]],\
+["mistralrs_core",["",[["engine",[],["mod.rs"]],["models",[],["gemma.rs","llama.rs","mistral.rs","mixtral.rs","mod.rs","quantized_llama.rs"]],["pipeline",[],["gemma.rs","llama.rs","mistral.rs","mixtral.rs","mod.rs"]],["utils",[],["mod.rs","tokens.rs","varbuilder_utils.rs"]],["xlora_models",[],["classifier.rs","config.rs","gemma.rs","llama.rs","mistral.rs","mixtral.rs","mod.rs","quantized_llama.rs"]]],["lib.rs","request.rs","response.rs","sampling.rs","scheduler.rs","sequence.rs"]]],\
+["mistralrs_lora",["",[],["frozenlinear.rs","lib.rs","loralinear.rs","qloralinear.rs"]]],\
 ["mistralrs_server",["",[],["main.rs","openai.rs"]]]\
 ]'));
 createSrcSidebar();
