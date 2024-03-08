6ccb8a32df3e95fa21d00987f6aabadf8613df2a
EricLBuehler <ericlbuehler@gmail.com>
2024-03-08 05:34:50 -0500

    Update simple

--- pyo3.py
+++ pyo3.py
@@ -10,5 +10,5 @@ loader = MistralLoader(
     quantized_filename="mistral-7b-instruct-v0.1.Q4_K_M.gguf",
 )
 runner=loader.load()
-res = runner.add_request(Request(messages = {"role": "user", "content": "Tell me a story about the Rust type system."}, max_tokens = 256, repetition_penalty = 1.0, top_p = 0.1, temperature = 0.1))
+res = runner.add_request(Request(model = "mistral", messages = {"role": "user", "content": "Tell me a story about the Rust type system."}, max_tokens = 256, repetition_penalty = 1.0, top_p = 0.1, temperature = 0.1))
 print(res)
