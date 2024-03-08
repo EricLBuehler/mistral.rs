36296a7a7cbce21a7905f1ceb0e1adbdbef89507
EricLBuehler <ericlbuehler@gmail.com>
2024-03-08 05:25:18 -0500

    Update simple example

--- pyo3.py
+++ pyo3.py
@@ -1,7 +1,7 @@
-import mistralrs
+from mistralrs import ModelKind, MistalLoader, Request
 
-kind = mistralrs.ModelKind.QuantizedGGUF
-loader = mistralrs.MistralLoader(
+kind = ModelKind.QuantizedGGUF
+loader = MistralLoader(
     model_id="mistralai/Mistral-7B-Instruct-v0.1",
     kind=kind,
     no_kv_cache=False,
@@ -10,3 +10,5 @@ loader = mistralrs.MistralLoader(
     quantized_filename="mistral-7b-instruct-v0.1.Q4_K_M.gguf",
 )
 runner=loader.load()
+res = runner.add_request(Request(messages = {"role": "user", "content": "Tell me a story about the Rust type system."}, max_tokens = 256, frequency_penalty = 1.0, top_p = 0.1, temperature = 0.1))
+print(res)
