f5b02e52d54e41d9324c62b20b53bc9602d0ca68
EricLBuehler <ericlbuehler@gmail.com>
2024-03-15 05:30:48 -0400

    Update shapes for qllama

--- mistralrs-core/src/models/quantized_llama.rs
+++ mistralrs-core/src/models/quantized_llama.rs
@@ -195,16 +195,13 @@ impl LayerWeights {
 
         let q = q
             .reshape((b_sz, seq_len, self.n_head, self.head_dim))?
-            .transpose(1, 2)?;
+            .transpose(1, 2)?.contiguous()?;
         let k = k
             .reshape((b_sz, seq_len, self.n_kv_head, self.head_dim))?
-            .transpose(1, 2)?;
+            .transpose(1, 2)?.contiguous()?;
         let v = v
             .reshape((b_sz, seq_len, self.n_kv_head, self.head_dim))?
-            .transpose(1, 2)?;
-
-        dbg!(q.is_contiguous());
-        dbg!(v.is_contiguous());
+            .transpose(1, 2)?.contiguous()?;
 
         //self.rotary.forward(start_offsets, &mut q, &mut k, false)?;
 
