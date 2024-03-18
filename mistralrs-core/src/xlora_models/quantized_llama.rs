b797429aa6b237161c7b61786b1587aefcb5caaf
EricLBuehler <ericlbuehler@gmail.com>
2024-03-18 17:42:24 -0400

    Debug

--- mistralrs-core/src/xlora_models/quantized_llama.rs
+++ mistralrs-core/src/xlora_models/quantized_llama.rs
@@ -753,6 +753,7 @@ impl ModelWeights {
         let (_b_size, seq_len_full) = input_ids_full.dims2()?;
         let (_, seq_len) = input_ids.dims2()?;
 
+        dbg!("Making scalings...");
         let scalings = self.get_scalings(
             input_ids,
             input_ids_full,
@@ -763,6 +764,7 @@ impl ModelWeights {
             no_kv_cache,
             non_granular_state,
         )?;
+        dbg!(&scalings);
 
         if no_kv_cache {
             self.inner_forward(
