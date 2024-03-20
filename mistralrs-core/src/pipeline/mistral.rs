b7d1d5fef1eab0a0ff39121d7cd5df979f48a164
EricLBuehler <ericlbuehler@gmail.com>
2024-03-20 06:43:09 -0400

    Formatting

--- mistralrs-core/src/pipeline/mistral.rs
+++ mistralrs-core/src/pipeline/mistral.rs
@@ -267,7 +267,7 @@ impl Loader for MistralLoader {
                 let vb = from_mmaped_safetensors(
                     paths.get_weight_filenames().to_vec(),
                     Vec::new(),
-                    DType::BF16,//dtype.unwrap_or(default_dtype),
+                    dtype.unwrap_or(default_dtype),
                     device,
                     false,
                 )?;
