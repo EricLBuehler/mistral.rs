171bf4df665e9ab1bea3c2049cb5d27afd4e35ef
EricLBuehler <ericlbuehler@gmail.com>
2024-03-22 06:02:44 -0400

    Verify

--- mistralrs-core/src/models/quantized_llama.rs
+++ mistralrs-core/src/models/quantized_llama.rs
@@ -247,10 +247,10 @@ impl LayerWeights {
             .reshape((b_sz, seq_len, self.n_kv_head, self.head_dim))?
             .transpose(1, 2)?
             .contiguous()?;
-        /*let v = v
+        let v = v
             .reshape((b_sz, seq_len, self.n_kv_head, self.head_dim))?
             .transpose(1, 2)?
-            .contiguous()?;*/
+            .contiguous()?;
 
 
 
