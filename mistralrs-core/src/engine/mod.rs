ce4b0ae9937e616051d46c62bfed473e638b8f29
EricLBuehler <ericlbuehler@gmail.com>
2024-03-26 19:50:54 -0400

    Debug possible memset

--- mistralrs-core/src/engine/mod.rs
+++ mistralrs-core/src/engine/mod.rs
@@ -65,8 +65,10 @@ impl Engine {
                 if !self.no_kv_cache {
                     self.clone_in_cache(&scheduled.completion);
                 }
+                println!("starting comple");
                 let logits =
                     get_mut_arcmutex!(self.pipeline).forward(scheduled.completion.clone(), false);
+                println!("done with comple");
                 let start = SystemTime::now()
                     .duration_since(UNIX_EPOCH)
                     .expect("Time travel has occurred!")
@@ -89,8 +91,10 @@ impl Engine {
             if scheduled.prompt.len() > 0 {
                 // Run the prompt seqs
                 self.set_none_cache();
+                println!("starting prompt");
                 let logits =
                     get_mut_arcmutex!(self.pipeline).forward(scheduled.prompt.clone(), true);
+                println!("done with prompt");
                 for seq in scheduled.prompt.iter() {
                     deref_mut_refcell!(seq).set_state(SequenceState::RunningCompletion);
                     let now = SystemTime::now()
