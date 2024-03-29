da06e07411d4c8c4b55d15dd422f0b78e987b9d5
EricLBuehler <ericlbuehler@gmail.com>
2024-03-29 15:29:36 -0400

    Remove q .contig

--- mistralrs-core/src/models/quantized_llama.rs
+++ mistralrs-core/src/models/quantized_llama.rs
@@ -199,8 +199,7 @@ impl LayerWeights {
         if q.rank() == 3 {
             q = q
                 .reshape((b_sz, seq_len, self.n_head, self.head_dim))?
-                .transpose(1, 2)?
-                .contiguous()?;
+                .transpose(1, 2)?;
             k = k
                 .reshape((b_sz, seq_len, self.n_kv_head, self.head_dim))?
                 .transpose(1, 2)?;
