e9a956708809bf388f3f0271b7a528e8203b72a5
Eric Buehler <ericlbuehler@gmail.com>
2024-04-03 00:52:49 +0000

    Remove comment

--- mistralrs-server/src/main.rs
+++ mistralrs-server/src/main.rs
@@ -92,7 +92,6 @@ impl futures::Stream for Streamer {
     type Item = Result<Event, axum::Error>;
 
     fn poll_next(mut self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        //return Poll::Ready(Some(Ok(Event::default().data(r#"{"id":"chatcmpl-123","object":"chat.completion.chunk","created":1694268190,"model":"gpt-3.5-turbo-0125", "system_fingerprint": "fp_44709d6fcb", "choices":[{"index":0,"delta":{"role":"assistant","content":""},"logprobs":null,"finish_reason":null}]}"#))));
         if self.is_done {
             return Poll::Ready(None);
         }
