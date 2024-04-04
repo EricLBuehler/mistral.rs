1ac0137ae185ef47c756b9682c135347f5df05a5
EricLBuehler <ericlbuehler@gmail.com>
2024-04-04 19:55:19 -0400

    Check cast

--- mistralrs-core/src/models/llama.rs
+++ mistralrs-core/src/models/llama.rs
@@ -182,7 +182,7 @@ impl CausalSelfAttention {
             let k = k.transpose(1, 2)?.to_dtype(DType::BF16)?;
             let v = v.transpose(1, 2)?.to_dtype(DType::BF16)?;
             let softmax_scale = 1f32 / (self.head_dim as f32).sqrt();
-            flash_attn(&q, &k, &v, softmax_scale, seq_len > 1)?
+            flash_attn(&q, &k, &v, softmax_scale, seq_len > 1).unwrap()
                 .transpose(1, 2)?
                 .to_dtype(DType::F32)?
         } else {
