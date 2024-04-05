083e35cb15ce93e2a3cdb7d0476d152fd9f3f9ab
EricLBuehler <65165915+EricLBuehler@users.noreply.github.com>
2024-04-05 00:55:45 +0000

    Deploying to gh-pages from @ EricLBuehler/mistral.rs@3fd1c3ed18fac9479701272fb3772d3983f89c7e 🚀

--- trait.impl/core/fmt/trait.Display.js
+++ trait.impl/core/fmt/trait.Display.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"mistralrs_core":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.77.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"mistralrs_core/enum.TokenSource.html\" title=\"enum mistralrs_core::TokenSource\">TokenSource</a>"]]
+"mistralrs_core":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.77.1/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"mistralrs_core/enum.TokenSource.html\" title=\"enum mistralrs_core::TokenSource\">TokenSource</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
