994f3a9322285b46a101df552dc0f99846ec4426
EricLBuehler <ericlbuehler@gmail.com>
2024-04-06 18:19:32 -0400

    Debug

--- mistralrs-core/src/models/quantized_phi.rs
+++ mistralrs-core/src/models/quantized_phi.rs
@@ -301,7 +301,7 @@ impl ModelWeights {
 
             // MLP
             let feed_forward_hidden_states = layer.ffn_up.forward(&x)?;
-            let feed_forward_hidden_states = self.act.forward(&feed_forward_hidden_states)?;
+            //let feed_forward_hidden_states = self.act.forward(&feed_forward_hidden_states)?;
             let feed_forward_hidden_states = layer.ffn_down.forward(&feed_forward_hidden_states)?;
             dbg!(feed_forward_hidden_states.mean_all());
             dbg!(residual.mean_all());
