a034ea5c73ad2aa93bc89cce59b70d9d55b2978d
EricLBuehler <65165915+EricLBuehler@users.noreply.github.com>
2024-04-08 23:25:23 +0000

    Deploying to gh-pages from @ EricLBuehler/mistral.rs@7344800169174aa806e30729b001b26b121de828 🚀

--- trait.impl/core/convert/trait.AsRef.js
+++ trait.impl/core/convert/trait.AsRef.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"mistralrs_core":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.77.1/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.77.1/std/primitive.str.html\">str</a>&gt; for <a class=\"enum\" href=\"mistralrs_core/enum.ModelKind.html\" title=\"enum mistralrs_core::ModelKind\">ModelKind</a>"]]
+"mistralrs_core":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.77.0/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.77.0/std/primitive.str.html\">str</a>&gt; for <a class=\"enum\" href=\"mistralrs_core/enum.ModelKind.html\" title=\"enum mistralrs_core::ModelKind\">ModelKind</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
