5ed198dc3b610ccc260f107bccc07cfc617b43a6
EricLBuehler <ericlbuehler@gmail.com>
2024-04-09 14:09:16 -0400

    Pure argmax

--- mistralrs-core/src/sampler.rs
+++ mistralrs-core/src/sampler.rs
@@ -94,10 +94,7 @@ impl Sampler {
                     (&logits / temperature)?
                 };
                 let logits = candle_nn::ops::softmax_last_dim(&logits)?;
-                let probs: Vec<f32> = logits.to_vec1()?;
-                let mut argsort_indices = (0..probs.len()).collect::<Vec<_>>();
-                // Sort by descending probability.
-                argsort_indices.sort_by(|&i, &j| probs[j].partial_cmp(&probs[i]).unwrap());
+                
 
                 // Apply topk, topp
                 //let logits = self.apply_topk_topp(logits)?;
