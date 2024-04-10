e2c40d2b87c64cd9e4413f9788b49bf5d585299c
EricLBuehler <65165915+EricLBuehler@users.noreply.github.com>
2024-04-10 02:40:59 +0000

    Deploying to gh-pages from @ EricLBuehler/mistral.rs@b0a1fb7a6ead713df31ccd396a566ca59747f05e 🚀

--- src-files.js
+++ src-files.js
@@ -2,6 +2,6 @@ var srcIndex = new Map(JSON.parse('[\
 ["mistralrs",["",[],["lib.rs"]]],\
 ["mistralrs_core",["",[["engine",[],["mod.rs"]],["models",[],["gemma.rs","llama.rs","mistral.rs","mixtral.rs","mod.rs","phi2.rs","quantized_llama.rs"]],["pipeline",[],["gemma.rs","llama.rs","mistral.rs","mixtral.rs","mod.rs","phi2.rs"]],["utils",[],["mod.rs","tokens.rs","varbuilder_utils.rs"]],["xlora_models",[],["classifier.rs","config.rs","gemma.rs","llama.rs","mistral.rs","mixtral.rs","mod.rs","phi2.rs","quantized_llama.rs"]]],["lib.rs","request.rs","response.rs","sampler.rs","scheduler.rs","sequence.rs"]]],\
 ["mistralrs_lora",["",[],["frozenlinear.rs","lib.rs","loralinear.rs","qloralinear.rs"]]],\
-["mistralrs_server",["",[],["main.rs","model_selected.rs","openai.rs"]]]\
+["mistralrs_server",["",[],["interactive_mode.rs","main.rs","model_selected.rs","openai.rs"]]]\
 ]'));
 createSrcSidebar();
