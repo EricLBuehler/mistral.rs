a7a16a13869e035c544c58522f894cd05276110d
EricLBuehler <ericlbuehler@gmail.com>
2024-04-20 17:47:44 -0400

    Update all the examples

--- examples/python/python_api.py
+++ examples/python/python_api.py
@@ -1,15 +1,15 @@
-from mistralrs import MistralLoader, QuantizedLoader, ChatCompletionRequest
+from mistralrs import Runner, Which, ChatCompletionRequest
 
-loader = QuantizedLoader(
-    MistralLoader,
-    is_gguf=True,
-    model_id="mistralai/Mistral-7B-Instruct-v0.1",
-    no_kv_cache=False,
-    repeat_last_n=64,
-    quantized_model_id="TheBloke/Mistral-7B-Instruct-v0.1-GGUF",
-    quantized_filename="mistral-7b-instruct-v0.1.Q4_K_M.gguf",
+runner = Runner(
+    which=Which.MistralGGUF(
+        tok_model_id="mistralai/Mistral-7B-Instruct-v0.1",
+        quantized_model_id="TheBloke/Mistral-7B-Instruct-v0.1-GGUF",
+        quantized_filename="mistral-7b-instruct-v0.1.Q4_K_M.gguf",
+        tokenizer_json=None,
+        repeat_last_n=64,
+    )
 )
-runner = loader.load()
+
 res = runner.send_chat_completion_request(
     ChatCompletionRequest(
         model="mistral",
--- examples/python/xlora_gemma.py
+++ examples/python/xlora_gemma.py
@@ -1,24 +1,24 @@
-from mistralrs import GemmaLoader, XLoraLoader, ChatCompletionRequest
+from mistralrs import Runner, Which, ChatCompletionRequest
 
-loader = XLoraLoader(
-    GemmaLoader,
-    model_id="google/gemma-7b-it",
-    no_kv_cache=False,
-    repeat_last_n=64,
-    xlora_model_id="lamm-mit/x-lora-gemma-7b",
-    order_file="gemma-ordering.json",
+runner = Runner(
+    which=Which.XLoraGemma(
+        model_id="google/gemma-7b-it",
+        tokenizer_json=None,
+        repeat_last_n=64,
+        xlora_model_id="lamm-mit/x-lora-gemma-7b",
+        order="orderings/xlora-gemma-paper-ordering.json",
+        tgt_non_granular_index=None,
+    )
 )
-runner = loader.load()
+
 res = runner.send_chat_completion_request(
     ChatCompletionRequest(
         model="mistral",
-        messages=[
-            {"role": "user", "content": "Tell me a story about the Rust type system."}
-        ],
+        messages=[{"role": "user", "content": "What is graphene?"}],
         max_tokens=256,
         presence_penalty=1.0,
         top_p=0.1,
-        temperature=0.1,
+        temperature=0.5,
     )
 )
 print(res)
