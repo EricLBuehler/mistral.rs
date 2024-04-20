5e4f4dd2fbd5fc9ddb7ff874b60da1a1c80aa15f
EricLBuehler <ericlbuehler@gmail.com>
2024-04-20 13:47:10 -0400

    Format

--- examples/python/xlora_zephyr.py
+++ examples/python/xlora_zephyr.py
@@ -1,24 +1,22 @@
 from mistralrs import Runner, Which, ChatCompletionRequest
 
-runner = Runner(which = 
-                Which.XLoraMistralGGUF(
-                    tok_model_id = "HuggingFaceH4/zephyr-7b-beta",
-                    quantized_model_id = "TheBloke/zephyr-7B-beta-GGUF",
-                    quantized_filename = "zephyr-7b-beta.Q4_0.gguf",
-                    tokenizer_json = None,
-                    repeat_last_n = 64,
-                    xlora_model_id = "lamm-mit/x-lora",
-                    order = "orderings/xlora-paper-ordering.json",
-                    tgt_non_granular_index = None,
-                )
-               )
-    
+runner = Runner(
+    which=Which.XLoraMistralGGUF(
+        tok_model_id="HuggingFaceH4/zephyr-7b-beta",
+        quantized_model_id="TheBloke/zephyr-7B-beta-GGUF",
+        quantized_filename="zephyr-7b-beta.Q4_0.gguf",
+        tokenizer_json=None,
+        repeat_last_n=64,
+        xlora_model_id="lamm-mit/x-lora",
+        order="orderings/xlora-paper-ordering.json",
+        tgt_non_granular_index=None,
+    )
+)
+
 res = runner.send_chat_completion_request(
     ChatCompletionRequest(
         model="mistral",
-        messages=[
-            {"role": "user", "content": "What is graphene?"}
-        ],
+        messages=[{"role": "user", "content": "What is graphene?"}],
         max_tokens=256,
         presence_penalty=1.0,
         top_p=0.1,
