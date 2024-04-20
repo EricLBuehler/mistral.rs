8054765257f4cec6b85ebd4972200af303e8091c
EricLBuehler <ericlbuehler@gmail.com>
2024-04-20 10:59:39 -0400

    Add an example

--- examples/python/xlora_zephyr.py
+++ examples/python/xlora_zephyr.py
@@ -1,24 +1,28 @@
-from mistralrs import MistralLoader, XLoraLoader, ChatCompletionRequest
+from mistralrs import Runner, Which, ChatCompletionRequest
 
-loader = XLoraLoader(
-    MistralLoader,
-    model_id="HuggingFaceH4/zephyr-7b-beta",
-    no_kv_cache=False,
-    repeat_last_n=64,
-    xlora_model_id="lamm-mit/x-lora",
-    order_file="xlora-paper-ordering.json",
-)
-runner = loader.load()
+runner = Runner(which = 
+                Which.XLoraMistralGGUF(
+                    tok_model_id = "HuggingFaceH4/zephyr-7b-beta",
+                    quantized_model_id = "TheBloke/zephyr-7B-beta-GGUF",
+                    quantized_filename = "zephyr-7b-beta.Q4_0.gguf",
+                    tokenizer_json = None,
+                    repeat_last_n = 64,
+                    xlora_model_id = "lamm-mit/x-lora",
+                    order = "orderings/xlora-paper-ordering.json",
+                    tgt_non_granular_index = None,
+                )
+               )
+    
 res = runner.send_chat_completion_request(
     ChatCompletionRequest(
         model="mistral",
         messages=[
-            {"role": "user", "content": "Tell me a story about the Rust type system."}
+            {"role": "user", "content": "What is graphene?"}
         ],
         max_tokens=256,
         presence_penalty=1.0,
         top_p=0.1,
-        temperature=0.1,
+        temperature=0.5,
     )
 )
 print(res)
