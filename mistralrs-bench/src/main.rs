e1c755c57a9964f88034a190270e9c461c768d2f
EricLBuehler <ericlbuehler@gmail.com>
2024-04-20 11:49:30 -0400

    Use value delim

--- mistralrs-bench/src/main.rs
+++ mistralrs-bench/src/main.rs
@@ -219,7 +219,7 @@ struct Args {
     n_gen: usize,
 
     /// Number of concurrent requests to run.
-    #[arg(long, short, value_parser, num_args = 1.., value_delimiter = ',', value_terminator = " ")]
+    #[arg(long, short, value_parser, num_args = 1.., value_delimiter = ',', use_value_delimiter = true)]
     concurrency: Vec<usize>,
 
     /// Number of times to repeat each test.
