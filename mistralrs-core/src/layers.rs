6344b92ebee18f1c071094121c8c603c4668ce6f
EricLBuehler <ericlbuehler@gmail.com>
2024-04-29 06:09:54 -0400

    Keep rope in f32 during init long ctxts

--- mistralrs-core/src/layers.rs
+++ mistralrs-core/src/layers.rs
@@ -167,13 +167,13 @@ impl PhiRotaryEmbedding {
                 .map(|i| 1f32 / cfg.rope_theta.powf(i as f64 / dim as f64) as f32)
                 .collect();
             let inv_freq_len = inv_freq.len();
-            let inv_freq = Tensor::from_vec(inv_freq, (1, inv_freq_len), dev)?.to_dtype(dtype)?;
+            let inv_freq = Tensor::from_vec(inv_freq, (1, inv_freq_len), dev)?;
             let t = Tensor::arange(0u32, max_seq_len as u32, dev)?
                 .to_dtype(dtype)?
                 .reshape((max_seq_len, 1))?;
             let freqs = t.matmul(&inv_freq)?;
-            let sin = freqs.sin()?;
-            let cos = freqs.cos()?;
+            let sin = freqs.sin()?.to_dtype(dtype)?;
+            let cos = freqs.cos()?.to_dtype(dtype)?;
             Ok(Self {
                 short_cos: cos,
                 short_sin: sin,
