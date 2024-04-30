e68444dc79d0ec1ead7cfa6b0053af75e9d24e94
EricLBuehler <ericlbuehler@gmail.com>
2024-04-30 18:02:24 -0400

    Update loading bar style

--- mistralrs-core/src/utils/varbuilder_utils.rs
+++ mistralrs-core/src/utils/varbuilder_utils.rs
@@ -1,20 +1,14 @@
 //! Utilities for creating a VarBuilder from a VarMap loaded from tensor storage formats.
 
-use std::{
-    collections::HashMap,
-    path::PathBuf,
-    sync::{Arc, Mutex},
-    thread,
-};
+use std::{collections::HashMap, path::PathBuf, thread};
 
 use candle_core::{DType, Device, Result, Tensor};
 use candle_nn::{
     var_builder::{SimpleBackend, VarBuilderArgs},
     VarBuilder,
 };
-use indicatif::MultiProgress;
 
-use crate::{get_mut_arcmutex, utils::new_progress_bar};
+use crate::utils::new_progress_bar;
 
 /// Load tensors into a VarBuilder backed by a VarMap using MmapedSafetensors.
 /// Set `silent` to not show a progress bar.
@@ -25,22 +19,12 @@ pub(crate) fn from_mmaped_safetensors<'a>(
     device: &Device,
 ) -> Result<VarBuilderArgs<'a, Box<dyn SimpleBackend>>> {
     let mut handles = Vec::new();
-    let multi = Arc::new(Mutex::new(MultiProgress::new()));
+    let bar = new_progress_bar((paths.len() + xlora_paths.len()) as u64);
     for path in paths {
-        fn load(
-            path: PathBuf,
-            device: Device,
-            dtype: DType,
-            multi: Arc<Mutex<MultiProgress>>,
-        ) -> Result<HashMap<String, Tensor>> {
+        fn load(path: PathBuf, device: Device, dtype: DType) -> Result<HashMap<String, Tensor>> {
             let mut accum = HashMap::new();
             let tensors = unsafe { candle_core::safetensors::MmapedSafetensors::new(path)? };
 
-            let bar = new_progress_bar(tensors.tensors().len() as u64);
-            {
-                let multi = get_mut_arcmutex!(multi);
-                multi.add(bar.clone());
-            }
             for (name, _) in tensors.tensors() {
                 let new_name = if name.contains("base_model.model.model") {
                     name.replace("base_model.model.model", "model")
@@ -52,14 +36,11 @@ pub(crate) fn from_mmaped_safetensors<'a>(
                     .to_device(&device)?
                     .to_dtype(dtype)?;
                 accum.insert(new_name, tensor);
-                bar.inc(1);
             }
-            bar.finish();
             Ok(accum)
         }
         let device = device.clone();
-        let multi = multi.clone();
-        handles.push(thread::spawn(move || load(path, device, dtype, multi)));
+        handles.push(thread::spawn(move || load(path, device, dtype)));
     }
     for (i, path) in xlora_paths.into_iter().enumerate() {
         fn load(
@@ -67,16 +48,10 @@ pub(crate) fn from_mmaped_safetensors<'a>(
             device: Device,
             dtype: DType,
             i: usize,
-            multi: Arc<Mutex<MultiProgress>>,
         ) -> Result<HashMap<String, Tensor>> {
             let mut accum = HashMap::new();
             let tensors = unsafe { candle_core::safetensors::MmapedSafetensors::new(path)? };
 
-            let bar = new_progress_bar(tensors.tensors().len() as u64);
-            {
-                let multi = get_mut_arcmutex!(multi);
-                multi.add(bar.clone());
-            }
             for (name, _) in tensors.tensors() {
                 if name.contains("internal_xlora_classifier") {
                     continue;
@@ -93,18 +68,23 @@ pub(crate) fn from_mmaped_safetensors<'a>(
                     .to_device(&device)?
                     .to_dtype(dtype)?;
                 accum.insert(new_name, tensor);
-                bar.inc(1);
             }
-            bar.finish();
             Ok(accum)
         }
         let device = device.clone();
-        let multi = multi.clone();
-        handles.push(thread::spawn(move || load(path, device, dtype, i, multi)));
+        handles.push(thread::spawn(move || load(path, device, dtype, i)));
     }
     let mut ws = HashMap::new();
-    // Wait until each finishes
-    while !handles.iter().all(|h| h.is_finished()) {}
+    // Wait until each finishes, moving along the progress bar.
+    let mut n_done = 0;
+    while n_done < handles.len() - 1 {
+        let done_count = handles.iter().filter(|h| h.is_finished()).count();
+        let new_done = done_count - n_done;
+
+        bar.inc(new_done as u64);
+        n_done = done_count
+    }
+    bar.finish();
     for h in handles {
         ws.extend(h.join().unwrap()?);
     }
