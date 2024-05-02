8aa14badc231dbb358397b566f98c5223e9fa284
EricLBuehler <ericlbuehler@gmail.com>
2024-05-01 21:30:11 -0400

    Finalize pypi uploader

--- mistralrs-pyo3/upload.py
+++ mistralrs-pyo3/upload.py
@@ -58,9 +58,10 @@ os.system("maturin sdist --out dist")
 if "PYPI_TOKEN" in os.environ:
     password = os.environ["PYPI_TOKEN"]
     print("Using PyPi token as environment variable")
+    target = "pypi"
 else:
     password = input("Enter API token: ")
-target = input("Enter target (testpypi or pypi): ")
+    target = input("Enter target (testpypi or pypi): ")
 print(f"🚀 Uploading to {target} in 5 seconds. Press <CTRL>-C to abort.")
 time.sleep(5)
 
