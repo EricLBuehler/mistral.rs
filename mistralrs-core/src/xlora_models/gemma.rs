0b3ba2c915178d5e5f040258eea569c826e00f65
EricLBuehler <ericlbuehler@gmail.com>
2024-05-10 21:14:19 -0400

    Fix deadlock

--- mistralrs-core/src/xlora_models/gemma.rs
+++ mistralrs-core/src/xlora_models/gemma.rs
@@ -567,6 +567,7 @@ impl XLoraModel {
         no_kv_cache: bool,
         is_scaling_pass: Option<f64>,
     ) -> Result<Tensor> {
+        let attention_mask = CausalMasker.make_causal_mask(input_ids, &self.cache)?;
         let mut cache = if is_full_pass {
             if no_kv_cache {
                 let mut new_cache = Vec::new();
@@ -580,7 +581,6 @@ impl XLoraModel {
         } else {
             self.cache.lock()
         };
-        let attention_mask = CausalMasker.make_causal_mask(input_ids, &self.cache)?;
         let xs = self.embed_tokens.forward(input_ids)?;
         let mut xs = (xs * (self.hidden_size as f64).sqrt())?;
         for (i, layer) in self.layers.iter_mut().enumerate() {
--- mistralrs-core/src/xlora_models/mistral.rs
+++ mistralrs-core/src/xlora_models/mistral.rs
@@ -559,6 +559,11 @@ impl XLoraModel {
         no_kv_cache: bool,
         is_scaling_pass: Option<f64>,
     ) -> Result<Tensor> {
+        let attention_mask = CausalMasker.make_causal_mask_with_sliding_window(
+            input_ids,
+            &self.cache,
+            self.sliding_window,
+        )?;
         let mut cache = if is_full_pass {
             if no_kv_cache {
                 let mut new_cache = Vec::new();
@@ -572,11 +577,6 @@ impl XLoraModel {
         } else {
             self.cache.lock()
         };
-        let attention_mask = CausalMasker.make_causal_mask_with_sliding_window(
-            input_ids,
-            &self.cache,
-            self.sliding_window,
-        )?;
         let mut xs = self.embed_tokens.forward(input_ids)?;
         for (i, layer) in self.layers.iter().enumerate() {
             xs = self.mapper.map(xs, i)?;
