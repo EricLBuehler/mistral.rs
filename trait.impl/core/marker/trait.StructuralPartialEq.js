18be2d6756a62899a3fe81968f3d44a30a6576c4
EricLBuehler <65165915+EricLBuehler@users.noreply.github.com>
2024-05-15 21:23:43 +0000

    Deploying to gh-pages from @ EricLBuehler/mistral.rs@3a79bc82e0e7ba59f320908316d128e63b689179 🚀

--- trait.impl/core/marker/trait.StructuralPartialEq.js
+++ trait.impl/core/marker/trait.StructuralPartialEq.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"mistralrs_core":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.77.2/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"struct\" href=\"mistralrs_core/struct.TopLogprob.html\" title=\"struct mistralrs_core::TopLogprob\">TopLogprob</a>"]]
+"mistralrs_core":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.78.0/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"struct\" href=\"mistralrs_core/struct.TopLogprob.html\" title=\"struct mistralrs_core::TopLogprob\">TopLogprob</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
