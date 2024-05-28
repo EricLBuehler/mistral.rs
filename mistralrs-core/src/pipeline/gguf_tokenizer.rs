b276c160189f8e900efd7213acb031b3061f82de
EricLBuehler <ericlbuehler@gmail.com>
2024-05-28 07:17:56 -0400

    Use unigram tokenizer for llama

--- mistralrs-core/src/pipeline/gguf_tokenizer.rs
+++ mistralrs-core/src/pipeline/gguf_tokenizer.rs
@@ -1,14 +1,6 @@
-use std::collections::HashMap;
-
 use anyhow::Result;
 use candle_core::quantized::gguf_file::Content;
-use tokenizers::{
-    decoders::{byte_fallback::ByteFallback, fuse::Fuse, sequence::Sequence, strip::Strip},
-    models::bpe::BpeBuilder,
-    normalizers::{self, Prepend, Replace},
-    processors::template::{self, Template, TemplateProcessing, Tokens},
-    AddedToken, DecoderWrapper, ModelWrapper, NormalizerWrapper, Tokenizer,
-};
+use tokenizers::{models::unigram::Unigram, ModelWrapper, Tokenizer};
 use tracing::info;
 
 pub fn convert_ggml_to_hf_tokenizer(content: &Content) -> Result<Tokenizer> {
@@ -37,6 +29,14 @@ pub fn convert_ggml_to_hf_tokenizer(content: &Content) -> Result<Tokenizer> {
                 })
                 .collect::<Vec<_>>()
         });
+    let scores = content.metadata.get("tokenizer.ggml.scores").map(|items| {
+        items
+            .to_vec()
+            .expect("GGUF tokenizer scores is not a vec.")
+            .iter()
+            .map(|t| t.to_f32().expect("GGUF score is not a f32."))
+            .collect::<Vec<_>>()
+    });
     let merges = content.metadata.get("tokenizer.ggml.merges").map(|items| {
         items
             .to_vec()
@@ -47,91 +47,35 @@ pub fn convert_ggml_to_hf_tokenizer(content: &Content) -> Result<Tokenizer> {
     });
 
     info!(
-        "Converting GGML tokenizer. Model: `{model}`, num tokens: {}, num added tokens: {}, num merges: {}",
+        "Converting GGML tokenizer. Model: `{model}`, num tokens: {}, num added tokens: {}, num merges: {}, num scores: {}",
         tokens.len(),
         added_tokens.as_ref().map(|x| x.len()).unwrap_or(0),
-        merges.as_ref().map(|x| x.len()).unwrap_or(0)
+        merges.as_ref().map(|x| x.len()).unwrap_or(0),
+        scores.as_ref().map(|x| x.len()).unwrap_or(0)
     );
     let unk = content.metadata["tokenizer.ggml.unknown_token_id"]
         .to_u32()
         .expect("GGUF unk token is not u32");
 
-    let eos = content.metadata["tokenizer.ggml.eos_token_id"]
+    let _eos = content.metadata["tokenizer.ggml.eos_token_id"]
         .to_u32()
         .expect("GGUF unk token is not u32");
 
-    let bos = content.metadata["tokenizer.ggml.bos_token_id"]
+    let _bos = content.metadata["tokenizer.ggml.bos_token_id"]
         .to_u32()
         .expect("GGUF unk token is not u32");
 
     let tokenizer = match model.as_str() {
-        "llama" | "replit" | "gpt2" | "rwkv" => {
-            // BPE, as seen in relevant tokenizer.json files
-            let bpe_builder = BpeBuilder::new().unk_token(tokens[unk as usize].clone());
-            info!("Loading as BPE tokenizer.");
-
-            let mut vocab = HashMap::new();
-            for (i, tok) in tokens.iter().enumerate() {
-                #[allow(clippy::cast_possible_truncation)]
-                vocab.insert(tok.clone(), i as u32);
+        "llama" => {
+            let scores =
+                scores.expect("Expect `tokenizer.ggml.scores` for `llama` unigram tokeizer.");
+            let mut vocab = Vec::new();
+            for (token, score) in tokens.into_iter().zip(scores) {
+                vocab.push((token, score as f64));
             }
-            let mut merges_vec = Vec::new();
-            if let Some(merges) = merges {
-                for tok in merges {
-                    let split = tok.splitn(2, ' ').collect::<Vec<_>>();
-                    merges_vec.push((split[0].to_string(), split[1].to_string()));
-                }
-            }
-            let bpe = bpe_builder
-                .vocab_and_merges(vocab, merges_vec)
-                .fuse_unk(true)
-                .build()
-                .map_err(anyhow::Error::msg)?;
-            let mut tokenizer = Tokenizer::new(ModelWrapper::BPE(bpe));
-            tokenizer.with_decoder(Sequence::new(vec![
-                DecoderWrapper::Replace(Replace::new("▁", " ").map_err(anyhow::Error::msg)?),
-                DecoderWrapper::ByteFallback(ByteFallback::default()),
-                DecoderWrapper::Fuse(Fuse::new()),
-                DecoderWrapper::Strip(Strip::new(' ', 1, 0)),
-            ]));
-            if let Some(added_tokens) = added_tokens {
-                for added_token in added_tokens {
-                    tokenizer.add_special_tokens(&[AddedToken::from(added_token, true)]);
-                }
-            }
-            tokenizer.add_special_tokens(&[AddedToken::from(tokens[bos as usize].clone(), true)]);
-            tokenizer.add_special_tokens(&[AddedToken::from(tokens[eos as usize].clone(), true)]);
-            tokenizer.add_special_tokens(&[AddedToken::from(tokens[unk as usize].clone(), true)]);
-
-            tokenizer.with_post_processor(
-                TemplateProcessing::builder()
-                    .special_tokens(Tokens::from(vec![template::SpecialToken::new(
-                        tokens[bos as usize].clone(),
-                        vec![bos],
-                        vec![tokens[bos as usize].clone()],
-                    )
-                    .map_err(anyhow::Error::msg)?]))
-                    .pair(
-                        Template::try_from(vec![
-                            tokens[bos as usize].clone(),
-                            "$A".to_string(),
-                            tokens[bos as usize].clone(),
-                            "$B:1".to_string(),
-                        ])
-                        .unwrap(),
-                    )
-                    .single(
-                        Template::try_from(vec![tokens[bos as usize].clone(), "$A".to_string()])
-                            .unwrap(),
-                    )
-                    .build()?,
-            );
-            tokenizer.with_normalizer(normalizers::Sequence::new(vec![
-                NormalizerWrapper::Prepend(Prepend::new("▁".to_string())),
-                NormalizerWrapper::Replace(Replace::new(" ", "▁").map_err(anyhow::Error::msg)?),
-            ]));
-            info!("Decoder is: {:?}", tokenizer.get_decoder());
-            tokenizer
+            let unigram =
+                Unigram::from(vocab, Some(unk as usize), true).map_err(anyhow::Error::msg)?;
+            Tokenizer::new(ModelWrapper::Unigram(unigram))
         }
         other => {
             anyhow::bail!("Tokenizer model `{other}` not supported.");
