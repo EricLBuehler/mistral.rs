98269bc21c3ea33e0a047b225b3d58ed322a3108
EricLBuehler <ericlbuehler@gmail.com>
2024-06-01 12:10:52 -0400

    Fix response

--- mistralrs-core/src/engine/mod.rs
+++ mistralrs-core/src/engine/mod.rs
@@ -290,7 +290,7 @@ impl Engine {
     async fn add_request(&mut self, request: NormalRequest) {
         let is_chat = matches!(
             request.messages,
-            RequestMessage::Chat(_) | RequestMessage::VisionChat(_)
+            RequestMessage::Chat(_) | RequestMessage::VisionChat { .. }
         );
         let echo_prompt = matches!(
             request.messages,
