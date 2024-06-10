0e5b22396d5a26d4e03c9b1f6c31d621575dd2ff
EricLBuehler <ericlbuehler@gmail.com>
2024-06-10 14:28:01 -0400

    Abstract dtype settings

---
+++ mistralrs-core/src/utils/normal.rs
@@ -0,0 +1,48 @@
+use anyhow::Result;
+use candle_core::{DType, Device};
+use tracing::info;
+
+#[derive(Clone, Copy, Default)]
+/// DType for the model.
+/// ## `Auto` rules
+/// - If CUDA device or CPU, use BF16
+/// - Fallback to F16
+pub enum NonQuantizedDType {
+    #[default]
+    Auto,
+    BF16,
+    F16,
+    F32,
+}
+
+/// Type which can be converted to a DType
+pub trait TryIntoDType {
+    fn try_into_dtype(&self, device: &Device) -> Result<DType>;
+}
+
+impl TryIntoDType for DType {
+    fn try_into_dtype(&self, _: &Device) -> Result<DType> {
+        info!("DType selected is {self:?}.");
+        if !matches!(self, DType::BF16 | DType::F32 | DType::F64 | DType::F16) {
+            anyhow::bail!("DType must be one of BF16, F16, F32, F64");
+        }
+        Ok(*self)
+    }
+}
+
+impl TryIntoDType for NonQuantizedDType {
+    fn try_into_dtype(&self, device: &Device) -> Result<DType> {
+        match self {
+            Self::Auto => {
+                if device.is_cuda() || device.is_cpu() {
+                    Ok(DType::BF16)
+                } else {
+                    Ok(DType::F32)
+                }
+            }
+            Self::BF16 => Ok(DType::BF16),
+            Self::F16 => Ok(DType::F16),
+            Self::F32 => Ok(DType::F32),
+        }
+    }
+}
