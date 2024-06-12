fae4d7eab3c675880a8f585ebdb3122a7a34119e
EricLBuehler <ericlbuehler@gmail.com>
2024-06-12 06:56:04 -0400

    Fix deps and add examples

---
+++ examples/python/phi3v_base64.py
@@ -0,0 +1,44 @@
+from mistralrs import Runner, Which, ChatCompletionRequest, VisionArchitecture
+import base64
+
+runner = Runner(
+    which=Which.VisionPlain(
+        model_id="microsoft/Phi-3-vision-128k-instruct",
+        tokenizer_json=None,
+        repeat_last_n=64,
+        arch=VisionArchitecture.Phi3V,
+    ),
+)
+
+FILENAME = "picture.jpg"
+with open(FILENAME, "rb") as image_file:
+    encoded_string = base64.b64encode(image_file.read())
+
+res = runner.send_chat_completion_request(
+    ChatCompletionRequest(
+        model="phi3v",
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": encoded_string,
+                        },
+                    },
+                    {
+                        "type": "text",
+                        "text": "<|image_1|>\nWhat is shown in this image?",
+                    },
+                ],
+            }
+        ],
+        max_tokens=256,
+        presence_penalty=1.0,
+        top_p=0.1,
+        temperature=0.1,
+    )
+)
+print(res.choices[0].message.content)
+print(res.usage)
