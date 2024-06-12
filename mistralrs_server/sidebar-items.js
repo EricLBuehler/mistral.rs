101e7cdb8ca1af16c6110b7ea82aec152b21a17c
EricLBuehler <65165915+EricLBuehler@users.noreply.github.com>
2024-06-12 13:02:18 +0000

    Deploying to gh-pages from @ EricLBuehler/mistral.rs@0728b33c5b7d7c5ce5c1ef0ef663e7a68e2acc67 🚀

--- mistralrs_server/sidebar-items.js
+++ mistralrs_server/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["activate_adapters","get_router","health","main","models","parse_isq","parse_token_source","re_isq"],"mod":["chat_completion","completions","interactive_mode","openai"],"struct":["AdapterActivationRequest","Args","ReIsqRequest"]};
+window.SIDEBAR_ITEMS = {"constant":["MB_TO_B","N_INPUT_SIZE"],"fn":["activate_adapters","get_router","health","main","models","parse_isq","parse_token_source","re_isq"],"mod":["chat_completion","completions","interactive_mode","openai"],"struct":["AdapterActivationRequest","Args","ReIsqRequest"]};
