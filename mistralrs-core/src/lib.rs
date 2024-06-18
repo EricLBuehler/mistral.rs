d5bc486098975d7327795f2510ac352f780f4f26
EricLBuehler <ericlbuehler@gmail.com>
2024-06-18 15:41:07 -0400

    Fix shadowing for quantized llama

--- mistralrs-core/src/lib.rs
+++ mistralrs-core/src/lib.rs
@@ -1,5 +1,4 @@
 #![deny(clippy::cast_possible_truncation, clippy::cast_precision_loss)]
-#![deny(clippy::shadow_same, clippy::shadow_reuse, clippy::shadow_unrelated)]
 
 use cublaslt::setup_cublas_lt_wrapper;
 use engine::Engine;
