72626e9c83e40e65031e5044394421316ea5da09
EricLBuehler <ericlbuehler@gmail.com>
2024-06-20 02:24:13 -0400

    Add examples

---
+++ examples/python/idefics2.py
@@ -0,0 +1,39 @@
+from mistralrs import Runner, Which, ChatCompletionRequest, VisionArchitecture
+
+runner = Runner(
+    which=Which.VisionPlain(
+        model_id="HuggingFaceM4/idefics2-8b-chatty",
+        tokenizer_json=None,
+        repeat_last_n=64,
+        arch=VisionArchitecture.Idefics2,
+    ),
+)
+
+res = runner.send_chat_completion_request(
+    ChatCompletionRequest(
+        model="idefics2",
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": "https://d2r55xnwy6nx47.cloudfront.net/uploads/2018/02/Ants_Lede1300.jpg"
+                        },
+                    },
+                    {
+                        "type": "text",
+                        "text": "What is shown in this image?",
+                    },
+                ],
+            },
+        ],
+        max_tokens=256,
+        presence_penalty=1.0,
+        top_p=0.1,
+        temperature=0.1,
+    )
+)
+print(res.choices[0].message.content)
+print(res.usage)
