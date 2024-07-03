f300f96a6585b32ef9dfb7a893282871235718a9
EricLBuehler <ericlbuehler@gmail.com>
2024-07-03 19:18:06 -0400

    Correct conditional comp

--- mistralrs-quant/src/gptq/mod.rs
+++ mistralrs-quant/src/gptq/mod.rs
@@ -1,10 +1,11 @@
-mod ffi;
 #[cfg(feature = "cuda")]
-mod gptq_cuda;
+mod ffi;
 #[cfg(not(feature = "cuda"))]
 mod gptq_cpu;
-
 #[cfg(feature = "cuda")]
-pub use gptq_cuda::GptQMatMul;
+mod gptq_cuda;
+
 #[cfg(not(feature = "cuda"))]
 pub use gptq_cpu::GptQMatMul;
+#[cfg(feature = "cuda")]
+pub use gptq_cuda::GptQMatMul;
