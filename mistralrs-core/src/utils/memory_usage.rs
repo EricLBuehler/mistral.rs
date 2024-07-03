3a8380c0a6507a3d8bfd592d1cb853434edfc270
Eric Buehler <65165915+EricLBuehler@users.noreply.github.com>
2024-07-03 15:21:03 -0400

    Add tracking of memory usage (#392)
    * Add tracking of memory usage
    
    * Complete memory usage
    
    * Clippy

---
+++ mistralrs-core/src/utils/memory_usage.rs
@@ -0,0 +1,27 @@
+use candle_core::{Device, Result};
+use sysinfo::System;
+
+const KB_TO_BYTES: usize = 1024;
+
+pub struct MemoryUsage;
+
+impl MemoryUsage {
+    pub fn get_memory_available(&self, device: &Device) -> Result<usize> {
+        match device {
+            Device::Cpu => {
+                let mut sys = System::new_all();
+                sys.refresh_cpu();
+                Ok(usize::try_from(sys.free_memory())? * KB_TO_BYTES)
+            }
+            #[cfg(feature = "cuda")]
+            Device::Cuda(_) => candle_core::cuda::cudarc::driver::result::mem_get_info().0,
+            #[cfg(not(feature = "cuda"))]
+            Device::Cuda(_) => {
+                candle_core::bail!("Cannot get memory available for CUDA device")
+            }
+            Device::Metal(_) => {
+                candle_core::bail!("Cannot get memory available for Metal device")
+            }
+        }
+    }
+}
