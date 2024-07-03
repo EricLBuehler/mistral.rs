a905c8f73deed414257e413bfb0528471f52c483
EricLBuehler <ericlbuehler@gmail.com>
2024-07-03 14:09:37 -0400

    Complete memory usage

--- mistralrs-core/src/utils/memory_usage.rs
+++ mistralrs-core/src/utils/memory_usage.rs
@@ -1,4 +1,4 @@
-use candle_core::Device;
+use candle_core::{Device, Result};
 use sysinfo::System;
 
 const KB_TO_BYTES: usize = 1024;
@@ -6,18 +6,21 @@ const KB_TO_BYTES: usize = 1024;
 pub struct MemoryUsage;
 
 impl MemoryUsage {
-    pub fn get_memory_available(&self, device: &Device) -> usize {
+    pub fn get_memory_available(&self, device: &Device) -> Result<usize> {
         match device {
             Device::Cpu => {
                 let mut sys = System::new_all();
                 sys.refresh_cpu();
-                sys.free_memory() as usize * KB_TO_BYTES
+                Ok(sys.free_memory() as usize * KB_TO_BYTES)
             }
+            #[cfg(feature = "cuda")]
+            Device::Cuda(_) => candle_core::cuda::cudarc::driver::result::mem_get_info().0,
+            #[cfg(not(feature = "cuda"))]
             Device::Cuda(_) => {
-                todo!()
+                candle_core::bail!("Cannot get memory available for CUDA device")
             }
             Device::Metal(_) => {
-                todo!()
+                candle_core::bail!("Cannot get memory available for Metal device")
             }
         }
     }
--- mistralrs-core/src/utils/mod.rs
+++ mistralrs-core/src/utils/mod.rs
@@ -1,6 +1,5 @@
 pub(crate) mod debug;
 pub(crate) mod gguf_metadata;
-#[cfg(feature = "memory_usage")]
 pub(crate) mod memory_usage;
 pub(crate) mod model_config;
 pub(crate) mod normal;
