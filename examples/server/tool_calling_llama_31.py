18b9f6d259bfaff6f03275d943d45e6c91fe3bb6
Eric Buehler <ericlbuehler@gmail.com>
2024-07-30 06:06:35 -0400

    Try to pass the call result back

--- examples/server/tool_calling_llama_31.py
+++ examples/server/tool_calling_llama_31.py
@@ -1,4 +1,5 @@
 import openai
+import json
 
 openai.api_key = "EMPTY"
 openai.base_url = "http://localhost:1234/v1/"
@@ -30,5 +31,28 @@ completion = openai.chat.completions.create(
     model="llama-31", messages=messages, tools=tools, tool_choice="auto"
 )
 
-print(completion.choices[0].message)
 print(completion.usage)
+
+tool_called = completion.choices[0].message.tool_calls[0].function
+
+
+def get_current_weather(unit: str = None, location: str = None) -> str:
+    return f"The weather in {location} is 40 degrees {unit}."
+
+
+if tool_called.name == "get_current_weather":
+    args = json.loads(tool_called.arguments)
+    result = get_current_weather(**args)
+    messages.append(
+        {
+            "role": "assistant",
+            "content": json.dumps({"name": tool_called.name, "parameters": args}),
+        }
+    )
+    messages.append({"role": "ipython", "content": result})
+    print(messages)
+    completion = openai.chat.completions.create(
+        model="llama-31", messages=messages, tools=tools, tool_choice="auto"
+    )
+    print(completion.usage)
+    print(completion.choices[0].message)
