249299bd32649517a6f24245166ea5f3c463a869
Eric Buehler <65165915+EricLBuehler@users.noreply.github.com>
2024-08-05 20:01:14 -0400

    Python file cleanup (#668)

--- examples/server/yacc.py
+++ examples/server/yacc.py
@@ -1,7 +1,6 @@
-import openai
+from openai import OpenAI
 
-openai.api_key = "EMPTY"
-openai.base_url = "http://localhost:1234/v1/"
+client = OpenAI(api_key="foobar", base_url="http://localhost:1234/v1/")
 
 with open("examples/server/c.y", "r") as f:
     c_yacc = f.read()
