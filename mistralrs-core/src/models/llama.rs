7bb3cf743d2c4dc70f701ccc2a37ccd0f673c546
joshpopelka20 <107133849+joshpopelka20@users.noreply.github.com>
2024-08-14 14:08:20 -0400

    change to copy

--- mistralrs-core/src/models/llama.rs
+++ mistralrs-core/src/models/llama.rs
@@ -433,7 +433,7 @@ impl Llama {
         // let chunk = x.clone();
         // chunks.push(chunk.to_device(&self.cuda_devices[0])?);
         let mut chunks: Vec<Tensor> = Vec::with_capacity(num_devices);
-        chunks.push(x.clone());
+        chunks.push(x.copy());
 
         let mut cache = self.kv_caches[0].lock();
         let mask = CausalMasker.make_causal_mask_as_attn_bias(
@@ -449,7 +449,7 @@ impl Llama {
         for (block_idx, block) in self.blocks.iter().enumerate() {
             // x = self.mapper.map(x, block_idx)?;
             // x = self.mapper.map(&chunks[0], block_idx)?;
-            x = self.mapper.map(chunks[0].clone(), block_idx)?;
+            x = self.mapper.map(chunks[0].copy(), block_idx)?;
             x = block.forward(
                 &x,
                 &mask.clone().map(|m| m.to_device(x.device()).unwrap()),
