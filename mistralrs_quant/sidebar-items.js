2a2ef3d29f8be54e7cd6583818c5792d382f3b27
EricLBuehler <65165915+EricLBuehler@users.noreply.github.com>
2024-08-14 20:09:39 +0000

    Deploying to gh-pages from @ EricLBuehler/mistral.rs@c6d8720b156b84323900bb3fbd9af498b599c205 🚀

--- mistralrs_quant/sidebar-items.js
+++ mistralrs_quant/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["QuantMethodConfig","QuantMethodType"],"fn":["gptq_linear","linear","linear_b","linear_no_bias"],"struct":["GgufMatMul","GptqMatMul","QuantizedConfig","UnquantLinear"],"trait":["QuantMethod"]};
+window.SIDEBAR_ITEMS = {"enum":["IsqType","QuantMethodConfig","QuantMethodType"],"fn":["gptq_linear","linear","linear_b","linear_no_bias"],"struct":["GgufMatMul","GptqLayer","QuantizedConfig","UnquantLinear"],"trait":["QuantMethod"]};
