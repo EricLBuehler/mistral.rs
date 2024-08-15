3abf162cbf7c70037c7799eefa8fa8953e6b00e9
Eric Buehler <ericlbuehler@gmail.com>
2024-08-14 21:29:39 -0400

    Clippy

--- mistralrs-core/src/pipeline/isq.rs
+++ mistralrs-core/src/pipeline/isq.rs
@@ -93,7 +93,7 @@ pub trait IsqModel {
                     .iter()
                     .map(|(q, _)| {
                         q.get_max_isq_cpu_threads(dtype)
-                            .map(|x| usize::from(x))
+                            .map(usize::from)
                             .unwrap_or(current_rayon_threads)
                     })
                     .min()
