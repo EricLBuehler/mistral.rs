fcd2ca8188cd153a0a9a9d234044411159feef8b
Will Eaton <me@wseaton.com>
2024-08-15 09:38:27 -0400

    fix null in tool type response

--- mistralrs-core/src/tools/response.rs
+++ mistralrs-core/src/tools/response.rs
@@ -1,15 +1,14 @@
 #[cfg_attr(feature = "pyo3_macros", pyo3::pyclass(eq, eq_int))]
 #[cfg_attr(feature = "pyo3_macros", pyo3(get_all))]
 #[derive(Clone, Debug, serde::Serialize, PartialEq)]
-#[serde(untagged)]
+#[serde(rename_all = "snake_case")]
 pub enum ToolCallType {
-    #[serde(rename = "function")]
     Function,
 }
 
 #[cfg_attr(feature = "pyo3_macros", pyo3::pyclass)]
 #[cfg_attr(feature = "pyo3_macros", pyo3(get_all))]
-#[derive(Clone, Debug, serde::Serialize, serde::Deserialize)]
+#[derive(Clone, Debug, serde::Serialize)]
 pub struct CalledFunction {
     pub name: String,
     pub arguments: String,
@@ -24,3 +23,40 @@ pub struct ToolCallResponse {
     pub tp: ToolCallType,
     pub function: CalledFunction,
 }
+
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use serde_json::json;
+
+    // test the serde of the ToolCallResponse enum 
+    #[test]
+    fn test_tool_call_response_serde() {
+        let tool_call_response = ToolCallResponse {
+            id: "1".to_string(),
+            tp: ToolCallType::Function,
+            function: CalledFunction {
+                name: "test".to_string(),
+                arguments: "test".to_string(),
+            },
+        };
+
+        let json = json!({
+            "id": "1",
+            "type": "function",
+            "function": {
+                "name": "test",
+                "arguments": "test"
+            }
+        });
+
+
+        let string_json = serde_json::to_string(&tool_call_response).unwrap();
+
+        assert_eq!(string_json, json.to_string());
+
+    }
+}
+
