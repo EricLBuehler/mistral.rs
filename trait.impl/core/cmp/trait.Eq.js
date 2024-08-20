6bd789d676acad271bf211bf4a421dda4d69c7d3
EricLBuehler <65165915+EricLBuehler@users.noreply.github.com>
2024-08-20 23:20:45 +0000

    Deploying to gh-pages from @ EricLBuehler/mistral.rs@754bb6af915e006844ff6d356242d4ed9fb96467 🚀

---
+++ trait.impl/core/cmp/trait.Eq.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"mistralrs_quant":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.80.1/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"mistralrs_quant/enum.IsqType.html\" title=\"enum mistralrs_quant::IsqType\">IsqType</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ trait.impl/core/hash/trait.Hash.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"mistralrs_quant":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.80.1/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"mistralrs_quant/enum.IsqType.html\" title=\"enum mistralrs_quant::IsqType\">IsqType</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
