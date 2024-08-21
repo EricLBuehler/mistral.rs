15feb9378895c01a656850d9690ef3f829016752
Eric Buehler <ericlbuehler@gmail.com>
2024-08-20 23:29:15 -0400

    Update exports

--- mistralrs-core/src/lib.rs
+++ mistralrs-core/src/lib.rs
@@ -71,7 +71,7 @@ pub use pipeline::{
     MistralLoader, MixtralLoader, ModelKind, ModelPaths, NormalLoader, NormalLoaderBuilder,
     NormalLoaderType, NormalSpecificConfig, Phi2Loader, Phi3Loader, Phi3VLoader, Qwen2Loader,
     SpeculativeConfig, SpeculativeLoader, SpeculativePipeline, Starcoder2Loader, TokenSource,
-    VisionLoader, VisionLoaderBuilder, VisionLoaderType, VisionModelLoader, VisionSpecificConfig,
+    VisionLoader, VisionLoaderBuilder, VisionLoaderType, VisionSpecificConfig,
 };
 pub use request::{Constraint, MessageContent, NormalRequest, Request, RequestMessage};
 pub use response::Response;
