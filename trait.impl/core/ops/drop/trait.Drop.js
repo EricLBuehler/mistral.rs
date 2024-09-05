521d73fe6aa449b245b436f9ea1e28c522e9a937
EricLBuehler <65165915+EricLBuehler@users.noreply.github.com>
2024-09-05 20:24:24 +0000

    Deploying to gh-pages from @ EricLBuehler/mistral.rs@a625394370e447f51e83aa7743cd76830dbce72b 🚀

--- trait.impl/core/ops/drop/trait.Drop.js
+++ trait.impl/core/ops/drop/trait.Drop.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"mistralrs_core":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.80.1/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"mistralrs_core/struct.MistralRs.html\" title=\"struct mistralrs_core::MistralRs\">MistralRs</a>"]]
+"mistralrs_core":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.81.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"mistralrs_core/struct.MistralRs.html\" title=\"struct mistralrs_core::MistralRs\">MistralRs</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
