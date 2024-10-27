8a5257568d3f5eb2666b05563117d8fc28aaebb9
EricLBuehler <65165915+EricLBuehler@users.noreply.github.com>
2024-10-27 00:30:11 +0000

    Deploying to gh-pages from @ EricLBuehler/mistral.rs@8e60821ed7616ec4ed3640c7ba29906c23ed35ae 🚀

--- trait.impl/core/ops/drop/trait.Drop.js
+++ trait.impl/core/ops/drop/trait.Drop.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["mistralrs_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"mistralrs_core/struct.MistralRs.html\" title=\"struct mistralrs_core::MistralRs\">MistralRs</a>"]]],["mistralrs_server",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"mistralrs_server/printer/struct.Printer.html\" title=\"struct mistralrs_server::printer::Printer\">Printer</a>"]]]]);
+    var implementors = Object.fromEntries([["mistralrs_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"mistralrs_core/struct.MistralRs.html\" title=\"struct mistralrs_core::MistralRs\">MistralRs</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[294,312]}
+//{"start":57,"fragment_lengths":[294]}
