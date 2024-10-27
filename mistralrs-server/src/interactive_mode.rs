8e60821ed7616ec4ed3640c7ba29906c23ed35ae
Eric Buehler <65165915+EricLBuehler@users.noreply.github.com>
2024-10-26 20:23:35 -0400

    Remove pretty-printer (#889)

--- mistralrs-server/src/interactive_mode.rs
+++ mistralrs-server/src/interactive_mode.rs
@@ -14,7 +14,7 @@ use std::{
 use tokio::sync::mpsc::channel;
 use tracing::{error, info};
 
-use crate::{printer::Printer, util};
+use crate::util;
 
 fn exit_handler() {
     std::process::exit(0);
@@ -179,8 +179,6 @@ async fn text_interactive_mode(mistralrs: Arc<MistralRs>, throughput: bool) {
 
         let mut assistant_output = String::new();
 
-        let mut printer = Printer::new();
-
         let start = Instant::now();
         let mut toks = 0;
         while let Some(resp) = rx.recv().await {
@@ -188,7 +186,7 @@ async fn text_interactive_mode(mistralrs: Arc<MistralRs>, throughput: bool) {
                 Response::Chunk(chunk) => {
                     let choice = &chunk.choices[0];
                     assistant_output.push_str(&choice.delta.content);
-                    printer.print_to_stdout(&choice.delta.content).unwrap();
+                    print!("{}", choice.delta.content);
                     toks += 3usize; // NOTE: we send toks every 3.
                     io::stdout().flush().unwrap();
                     if choice.finish_reason.is_some() {
@@ -363,8 +361,6 @@ async fn vision_interactive_mode(mistralrs: Arc<MistralRs>, throughput: bool) {
 
         let mut assistant_output = String::new();
 
-        let mut printer = Printer::new();
-
         let start = Instant::now();
         let mut toks = 0;
         while let Some(resp) = rx.recv().await {
@@ -372,7 +368,7 @@ async fn vision_interactive_mode(mistralrs: Arc<MistralRs>, throughput: bool) {
                 Response::Chunk(chunk) => {
                     let choice = &chunk.choices[0];
                     assistant_output.push_str(&choice.delta.content);
-                    printer.print_to_stdout(&choice.delta.content).unwrap();
+                    print!("{}", choice.delta.content);
                     toks += 3usize; // NOTE: we send toks every 3.
                     io::stdout().flush().unwrap();
                     if choice.finish_reason.is_some() {
