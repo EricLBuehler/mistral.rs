42c3a59cb4e5c787e4b707b3176fb40d65fb709b
Eric Buehler <ericlbuehler@gmail.com>
2024-10-27 21:13:19 -0400

    Refine wheel generation script

--- generate_wheels.sh
+++ generate_wheels.sh
@@ -1,9 +1,6 @@
-# docker run --rm -v .:/io ghcr.io/pyo3/maturin build --release -o wheels -m mistralrs-pyo3/Cargo.toml
+# MAC: Aarch64 Manylinux and OSX
 
 docker build -t wheelmaker:latest -f Dockerfile.manylinux .
-
-# Manylinux and OSX
-
 docker run --rm -v .:/io wheelmaker build --release -o wheels-cpu -m mistralrs-pyo3/Cargo.toml --interpreter python3.10
 docker run --rm -v .:/io wheelmaker build --release -o wheels-cpu -m mistralrs-pyo3/Cargo.toml --interpreter python3.11
 docker run --rm -v .:/io wheelmaker build --release -o wheels-cpu -m mistralrs-pyo3/Cargo.toml --interpreter python3.12
@@ -23,3 +20,21 @@ maturin build -o wheels-metal -m mistralrs-pyo3/Cargo.toml --interpreter python3
 maturin build -o wheels-accelerate -m mistralrs-pyo3/Cargo.toml --interpreter python3.10 --features accelerate
 maturin build -o wheels-accelerate -m mistralrs-pyo3/Cargo.toml --interpreter python3.11 --features accelerate
 maturin build -o wheels-accelerate -m mistralrs-pyo3/Cargo.toml --interpreter python3.12 --features accelerate
+
+# WINDOWS: x86_64 Manylinux
+
+maturin build -o wheels-cpu -m mistralrs-pyo3/Cargo.toml --interpreter python3.10
+maturin build -o wheels-cpu -m mistralrs-pyo3/Cargo.toml --interpreter python3.11
+maturin build -o wheels-cpu -m mistralrs-pyo3/Cargo.toml --interpreter python3.12
+
+# CUDA
+
+maturin build -o wheels-cuda -m mistralrs-pyo3/Cargo.toml --interpreter python3.10 --features cuda
+maturin build -o wheels-cuda -m mistralrs-pyo3/Cargo.toml --interpreter python3.11 --features cuda
+maturin build -o wheels-cuda -m mistralrs-pyo3/Cargo.toml --interpreter python3.12 --features cuda
+
+# MKL
+
+maturin build -o wheels-mkl -m mistralrs-pyo3/Cargo.toml --interpreter python3.10 --features mkl
+maturin build -o wheels-mkl -m mistralrs-pyo3/Cargo.toml --interpreter python3.11 --features mkl
+maturin build -o wheels-mkl -m mistralrs-pyo3/Cargo.toml --interpreter python3.12 --features mkl
