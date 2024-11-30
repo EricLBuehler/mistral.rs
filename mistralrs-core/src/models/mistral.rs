a7a741d3552f0a95ad2c7597765b621e9c0100c0
Eric Buehler <ericlbuehler@gmail.com>
2024-11-29 21:37:12 -0500

    Simpler

--- mistralrs-core/src/models/mistral.rs
+++ mistralrs-core/src/models/mistral.rs
@@ -708,34 +708,6 @@ impl IsqModel for Model {
         }
         Ok(names)
     }
-
-    fn begin_track_stats(&mut self) -> anyhow::Result<()> {
-        let layers = self
-            .get_layers()
-            .0
-            .into_iter()
-            .map(|(layer, _)| layer)
-            .collect::<Vec<_>>();
-        for layer in layers {
-            Arc::get_mut(layer).unwrap().begin_track_stats()?;
-        }
-        Ok(())
-    }
-
-    fn extract_imatrix_data(&mut self) -> candle_core::Result<HashMap<usize, Option<Vec<f32>>>> {
-        let layers = self
-            .get_layers()
-            .0
-            .into_iter()
-            .enumerate()
-            .map(|(i, (layer, _))| (i, layer))
-            .collect::<Vec<_>>();
-        let mut data = HashMap::new();
-        for (i, layer) in layers {
-            data.insert(i, Some(layer.end_track_stats()?.to_vec1::<f32>()?));
-        }
-        Ok(data)
-    }
 }
 
 impl NormalModel for Model {
--- mistralrs-core/src/pipeline/isq.rs
+++ mistralrs-core/src/pipeline/isq.rs
@@ -141,15 +141,35 @@ pub trait IsqModel {
     );
 
     /// This is used for imatrix generation internally. Begin stats tracking.
-    fn begin_track_stats(&mut self) -> Result<()> {
+    fn begin_track_stats(&mut self) -> anyhow::Result<()> {
         // TODO: make this required.
-        anyhow::bail!("This model does not support tracking stats.")
+        let layers = self
+            .get_layers()
+            .0
+            .into_iter()
+            .map(|(layer, _)| layer)
+            .collect::<Vec<_>>();
+        for layer in layers {
+            Arc::get_mut(layer).unwrap().begin_track_stats()?;
+        }
+        Ok(())
     }
 
     /// End stats tracking and return the imatrix data
     fn extract_imatrix_data(&mut self) -> candle_core::Result<HashMap<usize, Option<Vec<f32>>>> {
         // TODO: make this required.
-        candle_core::bail!("This model does not support quantizing with an imatrix.");
+        let layers = self
+            .get_layers()
+            .0
+            .into_iter()
+            .enumerate()
+            .map(|(i, (layer, _))| (i, layer))
+            .collect::<Vec<_>>();
+        let mut data = HashMap::new();
+        for (i, layer) in layers {
+            data.insert(i, Some(layer.end_track_stats()?.to_vec1::<f32>()?));
+        }
+        Ok(data)
     }
 
     /// Corresponds to `IsqOrganization::MoeExpertsOnly`
@@ -166,9 +186,18 @@ pub trait IsqModel {
 
     /// Corresponds to `IsqOrganization::MoeExpertsOnly`
     /// This is used for imatrix generation internally. Begin stats tracking.
-    fn begin_track_stats_moe_experts_only(&mut self) -> Result<()> {
+    fn begin_track_stats_moe_experts_only(&mut self) -> anyhow::Result<()> {
         // TODO: make this required.
-        anyhow::bail!("This model does not support tracking stats.")
+        let layers = self
+            .get_layers()
+            .0
+            .into_iter()
+            .map(|(layer, _)| layer)
+            .collect::<Vec<_>>();
+        for layer in layers {
+            Arc::get_mut(layer).unwrap().begin_track_stats()?;
+        }
+        Ok(())
     }
 
     /// Corresponds to `IsqOrganization::MoeExpertsOnly`
@@ -177,7 +206,18 @@ pub trait IsqModel {
         &mut self,
     ) -> candle_core::Result<HashMap<usize, Option<Vec<f32>>>> {
         // TODO: make this required.
-        candle_core::bail!("This model does not support quantizing with an imatrix.");
+        let layers = self
+            .get_layers()
+            .0
+            .into_iter()
+            .enumerate()
+            .map(|(i, (layer, _))| (i, layer))
+            .collect::<Vec<_>>();
+        let mut data = HashMap::new();
+        for (i, layer) in layers {
+            data.insert(i, Some(layer.end_track_stats()?.to_vec1::<f32>()?));
+        }
+        Ok(data)
     }
 
     /// Corresponding to the specific order the model produces ISQ layers (None means
