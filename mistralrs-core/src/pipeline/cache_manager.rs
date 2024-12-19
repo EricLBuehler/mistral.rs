c5821963fa1fb4508fbb577f1e39f21c12bc33b2
cdoko <190060110+cdoko@users.noreply.github.com>
2024-12-19 09:47:49 -0400

    Partial Fix for Sliding Window Attention (#994)

--- mistralrs-core/src/pipeline/cache_manager.rs
+++ mistralrs-core/src/pipeline/cache_manager.rs
@@ -195,10 +195,6 @@ impl KvCache {
                 if let Some(mut mask) = mask.cloned() {
                     let mask_len = mask.dim(1)?;
                     mask = mask.narrow(1, mask_len - (sliding_window - 1), sliding_window - 1)?;
-                    mask = Tensor::cat(
-                        &[&mask, &mask.narrow(1, mask_len - 1, 1)?.ones_like()?],
-                        D::Minus1,
-                    )?;
                     return Ok((k, v, Some(mask)));
                 }
             }
