1cf2fd4646fe94a1ada0481d7d482d90f518cf03
Eric Buehler <ericlbuehler@gmail.com>
2024-12-21 21:29:39 +0100

    Implement reshape_and_cache

--- mistralrs-paged-attn/src/metal/backend/paged_attention.rs
+++ mistralrs-paged-attn/src/metal/backend/paged_attention.rs
@@ -1,4 +1,6 @@
-use candle_core::{Result, Tensor};
+use candle_core::{DType, Device, Result, Storage, Tensor};
+
+use crate::metal::kernels::{self, ReshapeAndCacheDType};
 
 /// PagedAttention layer.
 ///
@@ -52,5 +54,134 @@ pub fn reshape_and_cache(
     value_cache: &Tensor,
     slot_mapping: &Tensor,
 ) -> Result<()> {
-    todo!()
+    let dtype = key.dtype();
+
+    let internal_type = match dtype {
+        DType::F16 => ReshapeAndCacheDType::F16,
+        DType::BF16 => ReshapeAndCacheDType::BF16,
+        DType::F32 => ReshapeAndCacheDType::F32,
+        dtype => candle_core::bail!("dtype {dtype:?} is not supported"),
+    };
+
+    let (k, k_l) = key.storage_and_layout();
+    let k = match &*k {
+        Storage::Metal(k) => k,
+        _ => candle_core::bail!("key must be a metal tensor"),
+    };
+
+    let (v, v_l) = value.storage_and_layout();
+    let v = match &*v {
+        Storage::Metal(v) => v,
+        _ => candle_core::bail!("value must be a metal tensor"),
+    };
+
+    let (kc, kc_l) = key_cache.storage_and_layout();
+    let kc = match &*kc {
+        Storage::Metal(kc) => kc,
+        _ => candle_core::bail!("key_cache must be a metal tensor"),
+    };
+
+    let (vc, vc_l) = value_cache.storage_and_layout();
+    let vc = match &*vc {
+        Storage::Metal(vc) => vc,
+        _ => candle_core::bail!("value_cache must be a metal tensor"),
+    };
+
+    let (s, s_l) = slot_mapping.storage_and_layout();
+    let s = match &*s {
+        Storage::Metal(s) => s,
+        _ => candle_core::bail!("slot_mapping must be a metal tensor"),
+    };
+
+    let k_rank = k_l.stride().len();
+    let v_rank = v_l.stride().len();
+    let kc_rank = kc_l.stride().len();
+    let vc_rank = vc_l.stride().len();
+
+    if k_rank != 3 || v_rank != 3 {
+        candle_core::bail!(
+            "paged-attention expects input tensors of rank 3 (k: {k_l:?}, v: {v_l:?})"
+        )
+    }
+
+    if kc_rank != 5 {
+        candle_core::bail!(
+            "paged-attention expects `key_cache` tensor to be of rank 5 \
+                (key_cache: {kc_l:?})"
+        )
+    }
+
+    if vc_rank != 4 {
+        candle_core::bail!(
+            "paged-attention expects `value_cache` tensor to be of rank 4 \
+                (value_cache: {vc_l:?})"
+        )
+    }
+
+    let (num_tokens, num_heads, head_size) = k_l.shape().dims3()?;
+    if (num_tokens, num_heads, head_size) != v_l.shape().dims3()? {
+        candle_core::bail!("shape mismatch k {:?} and v {:?}", k_l.shape(), v_l.shape())
+    }
+
+    let (num_blocks, num_heads_kc, head_size_kc, block_size, x) = kc_l.shape().dims5()?;
+    if num_heads_kc != num_heads || head_size_kc != head_size / x {
+        candle_core::bail!(
+            "shape mismatch value_cache {:?}, expected {:?}",
+            vc_l.shape(),
+            (num_blocks, num_heads, head_size / x, block_size, x)
+        )
+    }
+
+    if (num_blocks, num_heads, head_size, block_size) != vc_l.shape().dims4()? {
+        candle_core::bail!(
+            "shape mismatch key_cache {:?} and value_cache {:?}",
+            kc_l.shape(),
+            vc_l.shape()
+        )
+    }
+
+    if (num_tokens) != s_l.shape().dims1()? {
+        candle_core::bail!(
+            "shape mismatch slot_mapping {:?}, expected {:?}",
+            s_l.shape(),
+            (num_tokens)
+        )
+    }
+
+    let key_stride = k_l.stride()[0] as i32;
+    let value_stride = v_l.stride()[0] as i32;
+
+    let Device::Metal(dev) = key.device() else {
+        panic!("Expected the key to be on a Metal device.")
+    };
+
+    let command_buffer = dev.command_buffer()?;
+    command_buffer.set_label("reshape-and-cache");
+
+    kernels::call_reshape_and_cache(
+        dev.device(),
+        &command_buffer,
+        &kernels::Kernels::new(),
+        internal_type,
+        k.buffer(),
+        k_l.start_offset() * key.dtype().size_in_bytes(),
+        v.buffer(),
+        v_l.start_offset() * value.dtype().size_in_bytes(),
+        kc.buffer(),
+        kc_l.start_offset() * key_cache.dtype().size_in_bytes(),
+        vc.buffer(),
+        vc_l.start_offset() * value_cache.dtype().size_in_bytes(),
+        s.buffer(),
+        s_l.start_offset() * slot_mapping.dtype().size_in_bytes(),
+        num_tokens as i32,
+        num_heads as i32,
+        head_size as i32,
+        block_size as i32,
+        x as i32,
+        key_stride,
+        value_stride,
+    )
+    .map_err(candle_core::Error::wrap)?;
+
+    Ok(())
 }
