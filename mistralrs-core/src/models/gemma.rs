acea1fd727d3149a75bf67200fa408da0260682f
cdoko <190060110+cdoko@users.noreply.github.com>
2024-12-28 04:42:12 -0400

    Move start_offsets_kernel to correct device

--- mistralrs-core/src/models/gemma.rs
+++ mistralrs-core/src/models/gemma.rs
@@ -283,7 +283,8 @@ impl Attention {
             // Optimization for seqlen = 1, avoid transpose and just modify reshape dims
             v.reshape((b_sz, self.num_kv_heads, q_len, self.head_dim))?
         };
-
+        
+        let start_offsets_kernel = start_offsets_kernel.to_device(q.device())?;
         self.rotary_emb
             .forward(seqlen_offsets, &start_offsets_kernel, &mut q, &mut k, b_sz)?;
 
