095e28a656e4bdaf7d44b6dcd0515b26d5358076
cdoko <190060110+cdoko@users.noreply.github.com>
2024-12-31 04:57:18 -0400

    Add device mapper

--- mistralrs-core/src/vision_models/idefics3/inputs_processor.rs
+++ mistralrs-core/src/vision_models/idefics3/inputs_processor.rs
@@ -9,6 +9,7 @@ use tokenizers::Tokenizer;
 use tracing::warn;
 
 use crate::{
+    device_map::DeviceMapper,
     pipeline::{
         text_models_inputs_processor::{
             self, get_completion_input, get_prompt_input, PagedAttentionMeta,
@@ -112,6 +113,7 @@ impl InputsProcessor for Idefics3ImageProcessor {
         other_config: Option<Arc<dyn Any>>,
         mut paged_attn_metadata: Option<PagedAttentionMeta<'_>>,
         prompt_batchsize: Option<NonZeroUsize>,
+        _mapper: Option<&dyn DeviceMapper>,
     ) -> Box<dyn Iterator<Item = anyhow::Result<InputProcessorOutput>>> {
         if is_xlora {
             return Box::new(std::iter::once(Err(anyhow::Error::msg(
@@ -157,6 +159,7 @@ impl InputsProcessor for Idefics3ImageProcessor {
                 return_raw_logits,
                 paged_attn_metadata.as_mut(),
                 None, // TODO: evaluate if it is possible to batch this
+                None,
             )
             .nth(0)
             .unwrap()
@@ -174,6 +177,7 @@ impl InputsProcessor for Idefics3ImageProcessor {
                 return_raw_logits,
                 paged_attn_metadata.as_mut(),
                 None, // TODO: evaluate if it is possible to batch this
+                None,
             )
             .nth(0)
             .unwrap()
