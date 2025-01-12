b32b85d18e6b9f51847edc8908a90af2ad84af4f
Eric Buehler <65165915+EricLBuehler@users.noreply.github.com>
2025-01-12 14:58:59 -0500

    Include device memory avail in device map err (#1060)
    * Print device memory availability in err
    
    * Fix

--- mistralrs-core/src/pipeline/loaders/mod.rs
+++ mistralrs-core/src/pipeline/loaders/mod.rs
@@ -448,9 +448,14 @@ pub trait DeviceMappedModelLoader {
         if remaining_to_map > 0 {
             anyhow::bail!(
                 "This model does not fit on the devices {:?}, and exceeds total capacity by {}MB",
-                devices
+                per_layer_avail
                     .iter()
-                    .map(|dev| dev.device_pretty_repr())
+                    .rev()
+                    .map(|(avail, dev)| format!(
+                        "{} (avail: {}MB)",
+                        dev.device_pretty_repr(),
+                        avail / (1024 * 1024),
+                    ))
                     .collect::<Vec<_>>(),
                 remaining_to_map / (1024 * 1024)
             );
