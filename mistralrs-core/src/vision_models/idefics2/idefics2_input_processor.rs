66609022b84eb5a0da514d4970abe39578a63fd2
cdoko <190060110+cdoko@users.noreply.github.com>
2025-01-18 04:46:53 -0400

    pass mapper

--- mistralrs-core/src/vision_models/idefics2/idefics2_input_processor.rs
+++ mistralrs-core/src/vision_models/idefics2/idefics2_input_processor.rs
@@ -142,7 +142,7 @@ impl InputsProcessor for Idefics2ImageProcessor {
         other_config: Option<Arc<dyn Any>>,
         mut paged_attn_metadata: Option<PagedAttentionMeta<'_>>,
         prompt_batchsize: Option<NonZeroUsize>,
-        _mapper: Option<&dyn DeviceMapper>,
+        mapper: Option<&dyn DeviceMapper>,
     ) -> Box<dyn Iterator<Item = anyhow::Result<InputProcessorOutput>>> {
         if is_xlora {
             return Box::new(std::iter::once(Err(anyhow::Error::msg(
@@ -183,7 +183,7 @@ impl InputsProcessor for Idefics2ImageProcessor {
                 return_raw_logits,
                 paged_attn_metadata.as_mut(),
                 None, // TODO: evaluate if it is possible to batch this
-                None,
+                mapper,
             )
             .nth(0)
             .unwrap()
@@ -201,7 +201,7 @@ impl InputsProcessor for Idefics2ImageProcessor {
                 return_raw_logits,
                 paged_attn_metadata.as_mut(),
                 None, // TODO: evaluate if it is possible to batch this
-                None,
+                mapper,
             )
             .nth(0)
             .unwrap()
