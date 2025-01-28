5a3aed425a244ea6abd559cb27b249146c7c29b5
Eric Buehler <ericlbuehler@gmail.com>
2025-01-28 10:27:33 -0500

    Remove keep alive text

--- mistralrs-server/src/completions.rs
+++ mistralrs-server/src/completions.rs
@@ -260,11 +260,7 @@ pub async fn completions(
         };
 
         CompletionResponder::Sse(
-            Sse::new(streamer).keep_alive(
-                KeepAlive::new()
-                    .interval(Duration::from_millis(10))
-                    .text("keep-alive-text"),
-            ),
+            Sse::new(streamer).keep_alive(KeepAlive::new().interval(Duration::from_millis(10))),
         )
     } else {
         let response = match rx.recv().await {
