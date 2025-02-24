0560c1b3e96775178a396545d2e006e66ced33f6
EricLBuehler <65165915+EricLBuehler@users.noreply.github.com>
2025-02-24 04:10:25 +0000

    Deploying to gh-pages from @ EricLBuehler/mistral.rs@f83d843d9691899623546f7aaa597b19a58c98ce 🚀

--- mistralrs_quant/distributed/sidebar-items.js
+++ mistralrs_quant/distributed/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["get_global_tp_size_from_devices"],"mod":["layers","socket"],"struct":["Comm","Id","SumAllReduce"],"trait":["BarrierLike"]};
+window.SIDEBAR_ITEMS = {"fn":["get_global_tp_size_from_devices","use_nccl"],"mod":["layers","socket"],"struct":["Comm","Id","SumAllReduce"],"trait":["BarrierLike"]};
