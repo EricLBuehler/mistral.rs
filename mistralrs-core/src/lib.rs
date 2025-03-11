2e85ee34fc5b29464824124169758f15daf1a6e0
lucasj <lucasj@MacBook-Pro.local>
2025-03-11 11:30:46 +0800

    pub mod ops; is public

--- mistralrs-core/src/lib.rs
+++ mistralrs-core/src/lib.rs
@@ -32,7 +32,7 @@ pub mod device_map;
 mod engine;
 mod lora;
 mod model_loader;
-mod ops;
+pub mod ops;
 pub use model_loader::{
     get_auto_device_map_params, get_model_dtype, get_tgt_non_granular_index, LoaderBuilder,
 };
