a08d8ebaedb55f50a3089ea5a99df3d92ada9fc7
EricLBuehler <65165915+EricLBuehler@users.noreply.github.com>
2025-03-12 03:11:21 +0000

    Deploying to gh-pages from @ EricLBuehler/mistral.rs@d476535a5772d4e2211c81e2aacc2cfeaa830220 🚀

--- trait.impl/core/ops/deref/trait.Deref.js
+++ trait.impl/core/ops/deref/trait.Deref.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["mistralrs_server",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"mistralrs_server/openai/struct.MessageContent.html\" title=\"struct mistralrs_server::openai::MessageContent\">MessageContent</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"mistralrs_server/openai/struct.MessageInnerContent.html\" title=\"struct mistralrs_server::openai::MessageInnerContent\">MessageInnerContent</a>"]]]]);
+    var implementors = Object.fromEntries([["mistralrs",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"mistralrs/v0_4_api/struct.UqffTextModelBuilder.html\" title=\"struct mistralrs::v0_4_api::UqffTextModelBuilder\">UqffTextModelBuilder</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"mistralrs/v0_4_api/struct.UqffVisionModelBuilder.html\" title=\"struct mistralrs::v0_4_api::UqffVisionModelBuilder\">UqffVisionModelBuilder</a>"]]],["mistralrs_server",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"mistralrs_server/openai/struct.MessageContent.html\" title=\"struct mistralrs_server::openai::MessageContent\">MessageContent</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"mistralrs_server/openai/struct.MessageInnerContent.html\" title=\"struct mistralrs_server::openai::MessageInnerContent\">MessageInnerContent</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[663]}
+//{"start":57,"fragment_lengths":[663,664]}
---
+++ trait.impl/core/ops/deref/trait.DerefMut.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["mistralrs",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"mistralrs/v0_4_api/struct.UqffTextModelBuilder.html\" title=\"struct mistralrs::v0_4_api::UqffTextModelBuilder\">UqffTextModelBuilder</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"mistralrs/v0_4_api/struct.UqffVisionModelBuilder.html\" title=\"struct mistralrs::v0_4_api::UqffVisionModelBuilder\">UqffVisionModelBuilder</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[681]}
