aecf0fbce650b2b5e9f14c7c39788f587db9ba5c
Eric Buehler <65165915+EricLBuehler@users.noreply.github.com>
2025-03-28 21:07:11 -0400

    Format kernels (#1251)

--- mistralrs-quant/kernels/rotary/rotary.cu
+++ mistralrs-quant/kernels/rotary/rotary.cu
@@ -1,19 +1,15 @@
-#include <cuda_fp16.h>
 #include <cuda_bf16.h>
+#include <cuda_fp16.h>
 #include <stdint.h>
 
 #include "cuda_compat.h"
 
 namespace vllm {
 
-template<typename scalar_t, bool IS_NEOX>
+template <typename scalar_t, bool IS_NEOX>
 inline __device__ void apply_rotary_embedding(
-  scalar_t* __restrict__ arr,
-  const scalar_t* __restrict__ cos_ptr,
-  const scalar_t* __restrict__ sin_ptr,
-  int rot_offset,
-  int rot_dim)
-{
+    scalar_t *__restrict__ arr, const scalar_t *__restrict__ cos_ptr,
+    const scalar_t *__restrict__ sin_ptr, int rot_offset, int rot_dim) {
   int x_index, y_index;
   scalar_t cos, sin;
   if (IS_NEOX) {
@@ -36,23 +32,19 @@ inline __device__ void apply_rotary_embedding(
   arr[y_index] = y * cos + x * sin;
 }
 
-template<typename scalar_t, bool IS_NEOX>
+template <typename scalar_t, bool IS_NEOX>
 __global__ void rotary_embedding_kernel(
-  scalar_t* __restrict__ query,                    // [num_tokens, num_heads, head_size]
-  scalar_t* __restrict__ key,                      // [num_tokens, num_heads, head_size]
-  const scalar_t* __restrict__ cos_cache,          // [num_tokens, rot_dim]
-  const scalar_t* __restrict__ sin_cache,          // [num_tokens, rot_dim]
-  const int rot_dim,
-  const int64_t query_stride,
-  const int64_t key_stride,
-  const int num_heads,
-  const int num_kv_heads,
-  const int head_size) {
+    scalar_t *__restrict__ query, // [num_tokens, num_heads, head_size]
+    scalar_t *__restrict__ key,   // [num_tokens, num_heads, head_size]
+    const scalar_t *__restrict__ cos_cache, // [num_tokens, rot_dim]
+    const scalar_t *__restrict__ sin_cache, // [num_tokens, rot_dim]
+    const int rot_dim, const int64_t query_stride, const int64_t key_stride,
+    const int num_heads, const int num_kv_heads, const int head_size) {
   // Each thread block is responsible for one token.
   const int token_idx = blockIdx.x;
 
-  const scalar_t* cos_ptr = cos_cache + token_idx * rot_dim;
-  const scalar_t* sin_ptr = sin_cache + token_idx * rot_dim;
+  const scalar_t *cos_ptr = cos_cache + token_idx * rot_dim;
+  const scalar_t *sin_ptr = sin_cache + token_idx * rot_dim;
 
   const int nq = num_heads * rot_dim;
   for (int i = threadIdx.x; i < nq; i += blockDim.x) {
@@ -75,36 +67,25 @@ __global__ void rotary_embedding_kernel(
 
 } // namespace vllm
 
-#define CALL_ROTARY(T, IS_NEOX)                                               \
-  vllm::rotary_embedding_kernel<T, IS_NEOX><<<grid, block, 0, stream>>>(      \
-    reinterpret_cast<T*>(query),                                              \
-    reinterpret_cast<T*>(key),                                                \
-    reinterpret_cast<T*>(cos_cache),                                          \
-    reinterpret_cast<T*>(sin_cache),                                          \
-    rot_dim,                                                                  \
-    query_stride,                                                             \
-    key_stride,                                                               \
-    num_heads,                                                                \
-    num_kv_heads,                                                             \
-    head_size);
+#define CALL_ROTARY(T, IS_NEOX)                                                \
+  vllm::rotary_embedding_kernel<T, IS_NEOX><<<grid, block, 0, stream>>>(       \
+      reinterpret_cast<T *>(query), reinterpret_cast<T *>(key),                \
+      reinterpret_cast<T *>(cos_cache), reinterpret_cast<T *>(sin_cache),      \
+      rot_dim, query_stride, key_stride, num_heads, num_kv_heads, head_size);
 
-extern "C" void rotary_embedding(
-  void *query,             // [num_tokens, num_heads, head_size]
-  void *key,               // [num_tokens, num_kv_heads, head_size]
-  void *cos_cache,         // [num_tokens, rot_dim]
-  void *sin_cache,         // [num_tokens, rot_dim]
-  int32_t is_neox,
+extern "C" void
+rotary_embedding(void *query,     // [num_tokens, num_heads, head_size]
+                 void *key,       // [num_tokens, num_kv_heads, head_size]
+                 void *cos_cache, // [num_tokens, rot_dim]
+                 void *sin_cache, // [num_tokens, rot_dim]
+                 int32_t is_neox,
 
-  int32_t head_size,
-  int64_t num_tokens,
-  int32_t rot_dim,
-  int32_t num_heads,
-  int32_t num_kv_heads,
-  int64_t query_stride,
-  int64_t key_stride,
+                 int32_t head_size, int64_t num_tokens, int32_t rot_dim,
+                 int32_t num_heads, int32_t num_kv_heads, int64_t query_stride,
+                 int64_t key_stride,
 
-  uint32_t dtype // 0 => f16; 1 => bf16; 2 => f32
-  ) {
+                 uint32_t dtype // 0 => f16; 1 => bf16; 2 => f32
+) {
 
   dim3 grid(num_tokens);
   dim3 block(std::min(num_heads * rot_dim, 512));
@@ -112,7 +93,7 @@ extern "C" void rotary_embedding(
   const bool is_neox_bool = is_neox;
 
   if (is_neox_bool) {
-    if (dtype == 0){
+    if (dtype == 0) {
       CALL_ROTARY(half, true);
     } else if (dtype == 1) {
       CALL_ROTARY(__nv_bfloat16, true);
@@ -120,7 +101,7 @@ extern "C" void rotary_embedding(
       CALL_ROTARY(float, true);
     }
   } else {
-    if (dtype == 0){
+    if (dtype == 0) {
       CALL_ROTARY(half, false);
     } else if (dtype == 1) {
       CALL_ROTARY(__nv_bfloat16, false);
