1b88d304aa14d5d853ef2cc8c7355485ea515236
Eric Buehler <ericlbuehler@gmail.com>
2025-05-01 14:25:00 -0400

    Add speech pipeline

---
+++ mistralrs-core/src/pipeline/speech.rs
@@ -0,0 +1,361 @@
+use super::text_models_inputs_processor::PagedAttentionMeta;
+use super::{
+    AdapterPaths, AnyMoePipelineMixin, Cache, CacheManagerMixin, EitherCache, ForwardInputsResult,
+    GeneralMetadata, InputProcessorOutput, InputsProcessor, InputsProcessorType, IsqPipelineMixin,
+    Loader, MessagesAction, MetadataMixin, ModelCategory, ModelKind, ModelPaths,
+    PreProcessingMixin, Processor, TokenSource,
+};
+use crate::device_map::DeviceMapper;
+use crate::paged_attention::AttentionImplementation;
+use crate::pipeline::ChatTemplate;
+use crate::prefix_cacher::PrefixCacheManagerV2;
+use crate::sequence::Sequence;
+use crate::speech_models::{DiaConfig, DiaPipeline};
+use crate::utils::varbuilder_utils::DeviceForLoadTensor;
+use crate::utils::{tokens::get_token, varbuilder_utils::from_mmaped_safetensors};
+use crate::{
+    api_get_file, DeviceMapSetting, MessageContent, PagedAttentionConfig, Pipeline, TryIntoDType,
+};
+use anyhow::Result;
+use candle_core::{DType, Device, Tensor};
+use hf_hub::{api::sync::ApiBuilder, Repo, RepoType};
+use image::{DynamicImage, RgbImage};
+use indexmap::IndexMap;
+use mistralrs_quant::IsqType;
+use rand_isaac::Isaac64Rng;
+use std::any::Any;
+use std::num::NonZeroUsize;
+use std::path::PathBuf;
+use std::sync::Arc;
+use tokenizers::Tokenizer;
+use tokio::sync::Mutex;
+use tracing::{info, warn};
+
+#[derive(Clone, Debug)]
+pub struct SpeechModelPaths {
+    weights: Vec<PathBuf>,
+    config: PathBuf,
+}
+
+impl ModelPaths for SpeechModelPaths {
+    fn get_config_filename(&self) -> &PathBuf {
+        &self.config
+    }
+    fn get_tokenizer_filename(&self) -> &PathBuf {
+        unreachable!("Use `std::any::Any`.")
+    }
+    fn get_weight_filenames(&self) -> &[PathBuf] {
+        &self.weights
+    }
+    fn get_template_filename(&self) -> &Option<PathBuf> {
+        unreachable!("Use `std::any::Any`.")
+    }
+    fn get_gen_conf_filename(&self) -> Option<&PathBuf> {
+        unreachable!("Use `std::any::Any`.")
+    }
+    fn get_preprocessor_config(&self) -> &Option<PathBuf> {
+        unreachable!("Use `std::any::Any`.")
+    }
+    fn get_processor_config(&self) -> &Option<PathBuf> {
+        unreachable!("Use `std::any::Any`.")
+    }
+    fn get_chat_template_explicit(&self) -> &Option<PathBuf> {
+        unreachable!("Use `std::any::Any`.")
+    }
+    fn get_adapter_paths(&self) -> &AdapterPaths {
+        unreachable!("Use `std::any::Any`.")
+    }
+}
+
+pub struct SpeechProcessor;
+
+impl Processor for SpeechProcessor {
+    fn process(
+        &self,
+        _pipeline: &dyn Pipeline,
+        _messages: Vec<IndexMap<String, MessageContent>>,
+        _add_generation_prompt: bool,
+        _add_special_tokens: bool,
+        _enable_thinking: Option<bool>,
+        _tools: Vec<crate::Tool>,
+    ) -> Result<(Vec<u32>, String)> {
+        anyhow::bail!(
+            "SpeechProcessor::process should not be used. It does not expect chat messages."
+        )
+    }
+    fn inputs_processor(&self) -> Arc<dyn InputsProcessor> {
+        Arc::new(SpeechInputsProcessor)
+    }
+    fn get_special_tokens(&self) -> &[&'static str] {
+        &[]
+    }
+    fn template_action(&self) -> MessagesAction {
+        // Just a default
+        MessagesAction::FlattenOnlyText
+    }
+}
+
+pub struct SpeechInputsProcessor;
+
+#[derive(Clone)]
+pub struct ModelInputs {
+    pub(crate) prompt: String,
+}
+
+impl InputsProcessor for SpeechInputsProcessor {
+    fn get_type(&self) -> InputsProcessorType {
+        InputsProcessorType::Text
+    }
+
+    fn process_inputs(
+        &self,
+        _tokenizer: Option<Arc<Tokenizer>>,
+        input_seqs: &mut [&mut Sequence],
+        _is_prompt: bool,
+        _is_xlora: bool,
+        _device: &Device,
+        _no_kv_cache: bool,
+        _last_n_context_len: Option<(usize, usize)>,
+        _return_raw_logits: bool,
+        _other_config: Option<Arc<dyn Any>>,
+        _paged_attn_metadata: Option<PagedAttentionMeta<'_>>,
+        prompt_chunksize: Option<NonZeroUsize>,
+        _mapper: Option<&dyn DeviceMapper>,
+    ) -> Box<dyn Iterator<Item = Result<InputProcessorOutput>>> {
+        let make_value = if prompt_chunksize.is_some() {
+            return Box::new(std::iter::once(Err(anyhow::Error::msg(
+                "Prompt batching is unsupported for diffusion models",
+            ))));
+        } else {
+            || {
+                let inputs = ModelInputs {
+                    prompt: input_seqs[0].get_initial_prompt().to_string(),
+                };
+                Ok(InputProcessorOutput {
+                    inputs: Box::new(inputs),
+                    seq_indices: (0..input_seqs.len()).collect::<Vec<_>>(),
+                })
+            }
+        };
+        Box::new(std::iter::once(make_value()))
+    }
+}
+
+pub struct SpeechPipeline {
+    model: DiaPipeline,
+    metadata: Arc<GeneralMetadata>,
+    dummy_cache: EitherCache,
+}
+
+pub struct SpeechLoader;
+
+impl Loader for SpeechLoader {
+    #[allow(clippy::type_complexity, clippy::too_many_arguments)]
+    fn load_model_from_hf(
+        &self,
+        revision: Option<String>,
+        token_source: TokenSource,
+        dtype: &dyn TryIntoDType,
+        device: &Device,
+        silent: bool,
+        mapper: DeviceMapSetting,
+        in_situ_quant: Option<IsqType>,
+        paged_attn_config: Option<PagedAttentionConfig>,
+    ) -> Result<Arc<Mutex<dyn Pipeline + Send + Sync>>> {
+        let paths: anyhow::Result<Box<dyn ModelPaths>> = {
+            let api = ApiBuilder::new()
+                .with_progress(!silent)
+                .with_token(get_token(&token_source)?)
+                .build()?;
+            let revision = revision.unwrap_or("main".to_string());
+            let model_id = "nari-labs/Dia-1.6B";
+            let api = api.repo(Repo::with_revision(
+                model_id.to_string(),
+                RepoType::Model,
+                revision.clone(),
+            ));
+            let model_id = std::path::Path::new(model_id);
+
+            let weight = api_get_file!(api, "model.safetensors", &model_id);
+            let config = api_get_file!(api, "config.json", &model_id);
+            Ok(Box::new(SpeechModelPaths {
+                weights: vec![weight],
+                config,
+            }))
+        };
+        self.load_model_from_path(
+            &paths?,
+            dtype,
+            device,
+            silent,
+            mapper,
+            in_situ_quant,
+            paged_attn_config,
+        )
+    }
+
+    #[allow(clippy::type_complexity, clippy::too_many_arguments)]
+    fn load_model_from_path(
+        &self,
+        paths: &Box<dyn ModelPaths>,
+        dtype: &dyn TryIntoDType,
+        device: &Device,
+        silent: bool,
+        mapper: DeviceMapSetting,
+        in_situ_quant: Option<IsqType>,
+        mut paged_attn_config: Option<PagedAttentionConfig>,
+    ) -> Result<Arc<Mutex<dyn Pipeline + Send + Sync>>> {
+        let paths = &paths
+            .as_ref()
+            .as_any()
+            .downcast_ref::<SpeechModelPaths>()
+            .expect("Path downcast failed.");
+
+        if matches!(mapper, DeviceMapSetting::Map(_)) {
+            anyhow::bail!("Device mapping is not supported for speech models.")
+        }
+
+        if in_situ_quant.is_some() {
+            anyhow::bail!("ISQ is not supported for speech models.");
+        }
+
+        if paged_attn_config.is_some() {
+            warn!("PagedAttention is not supported for speech models, disabling it.");
+
+            paged_attn_config = None;
+        }
+
+        let cfg: DiaConfig = serde_json::from_str(&std::fs::read_to_string(&paths.config)?)?;
+
+        let mapper = DeviceMapSetting::dummy().into_mapper(usize::MAX, device, None)?;
+        let dtype = mapper.get_min_dtype(dtype)?;
+
+        let dtype = DType::F32;
+
+        let vb = from_mmaped_safetensors(
+            vec![paths.weights[0].clone()],
+            Vec::new(),
+            Some(dtype),
+            device,
+            vec![None],
+            silent,
+            None,
+            |_| true,
+            Arc::new(|_| DeviceForLoadTensor::Base),
+        )?;
+
+        let model = DiaPipeline::new(&cfg, vb)?;
+
+        Ok(Arc::new(Mutex::new(SpeechPipeline {
+            model,
+            metadata: Arc::new(GeneralMetadata {
+                max_seq_len: 1024,
+                llg_factory: None,
+                is_xlora: false,
+                no_prefix_cache: false,
+                num_hidden_layers: 1, // FIXME(EricLBuehler): we know this is only for caching, so its OK.
+                eos_tok: vec![],
+                kind: ModelKind::Normal,
+                no_kv_cache: true, // NOTE(EricLBuehler): no cache for these.
+                activation_dtype: dtype,
+                sliding_window: None,
+                cache_config: None,
+                cache_engine: None,
+                prompt_chunksize: None,
+                model_metadata: None,
+            }),
+            dummy_cache: EitherCache::Full(Cache::new(0, false)),
+        })))
+    }
+
+    fn get_id(&self) -> String {
+        "TODO".to_string()
+    }
+
+    fn get_kind(&self) -> ModelKind {
+        ModelKind::Normal
+    }
+}
+
+impl PreProcessingMixin for SpeechPipeline {
+    fn get_processor(&self) -> Arc<dyn Processor> {
+        Arc::new(SpeechProcessor)
+    }
+    fn get_chat_template(&self) -> Option<Arc<ChatTemplate>> {
+        None
+    }
+    fn get_input_processor_config(&self) -> Option<Arc<dyn Any>> {
+        None
+    }
+}
+
+impl IsqPipelineMixin for SpeechPipeline {
+    fn re_isq_model(&mut self, _dtype: IsqType) -> Result<()> {
+        anyhow::bail!("Speech models do not support ISQ for now.")
+    }
+}
+
+impl CacheManagerMixin for SpeechPipeline {
+    fn clone_in_cache(&self, _seqs: &mut [&mut Sequence]) {}
+    fn clone_out_cache(&self, _seqs: &mut [&mut Sequence]) {}
+    fn set_none_cache(
+        &self,
+        _seqs: &mut [&mut Sequence],
+        _reset_non_granular: bool,
+        _modify_draft_cache: bool,
+        _load_preallocated_cache: bool,
+    ) {
+    }
+    fn cache(&self) -> &EitherCache {
+        &self.dummy_cache
+    }
+}
+
+impl MetadataMixin for SpeechPipeline {
+    fn device(&self) -> Device {
+        self.model.device().clone()
+    }
+    fn get_metadata(&self) -> Arc<GeneralMetadata> {
+        self.metadata.clone()
+    }
+    fn name(&self) -> String {
+        "TODO".to_string()
+    }
+    fn reset_non_granular_state(&self) {}
+    fn tokenizer(&self) -> Option<Arc<Tokenizer>> {
+        None
+    }
+    fn device_mapper(&self) -> Option<&dyn DeviceMapper> {
+        None
+    }
+}
+
+#[async_trait::async_trait]
+impl Pipeline for SpeechPipeline {
+    fn forward_inputs(
+        &mut self,
+        inputs: Box<dyn Any>,
+        return_raw_logits: bool,
+    ) -> candle_core::Result<ForwardInputsResult> {
+        assert!(!return_raw_logits);
+
+        let ModelInputs { prompt } = *inputs.downcast().expect("Downcast failed.");
+        self.model.generate(&prompt)?;
+        todo!()
+    }
+    async fn sample_causal_gen(
+        &self,
+        _seqs: &mut [&mut Sequence],
+        _logits: Vec<Tensor>,
+        _prefix_cacher: &mut PrefixCacheManagerV2,
+        _disable_eos_stop: bool,
+        _srng: Arc<std::sync::Mutex<Isaac64Rng>>,
+    ) -> Result<(), candle_core::Error> {
+        candle_core::bail!("`sample_causal_gen` is incompatible with `SpeechPipeline`");
+    }
+    fn category(&self) -> ModelCategory {
+        ModelCategory::Speech
+    }
+}
+
+impl AnyMoePipelineMixin for SpeechPipeline {}
