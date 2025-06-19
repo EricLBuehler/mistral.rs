24ddeee632bc9f09f34f34f186d9f3bafaa99fe6
Eric Buehler <65165915+EricLBuehler@users.noreply.github.com>
2025-06-19 15:53:32 -0400

    Make bos/eos token IDs optional (#1493)

--- mistralrs-core/src/pipeline/vision.rs
+++ mistralrs-core/src/pipeline/vision.rs
@@ -539,10 +539,9 @@ impl Loader for VisionLoader {
             Some(processor.get_special_tokens()),
         )?;
 
-        let gen_conf: Option<GenerationConfig> = paths.get_gen_conf_filename().map(|f| {
-            serde_json::from_str(&fs::read_to_string(f).unwrap())
-                .expect("bos_token_id/eos_token_id missing in generation_config.json")
-        });
+        let gen_conf: Option<GenerationConfig> = paths
+            .get_gen_conf_filename()
+            .map(|f| serde_json::from_str(&fs::read_to_string(f).unwrap()).unwrap());
         let chat_template_explicit = paths
             .get_chat_template_explicit()
             .as_ref()
