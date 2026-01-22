6c77156004a94910f5618591b91a4576e17d74fb
EricLBuehler <65165915+EricLBuehler@users.noreply.github.com>
2026-01-22 21:17:08 +0000

    Deploying to gh-pages from @ EricLBuehler/mistral.rs@51aceeaf3f902cf8dfe4a7edf56bf0b3fb5bba5d 🚀

--- trait.impl/core/hash/trait.Hash.js
+++ trait.impl/core/hash/trait.Hash.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["mistralrs_quant",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"mistralrs_quant/enum.IsqType.html\" title=\"enum mistralrs_quant::IsqType\">IsqType</a>"]]]]);
+    var implementors = Object.fromEntries([["mistralrs_quant",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"mistralrs_quant/enum.IsqType.html\" title=\"enum mistralrs_quant::IsqType\">IsqType</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- trait.impl/core/ops/drop/trait.Drop.js
+++ trait.impl/core/ops/drop/trait.Drop.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["mistralrs_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"mistralrs_core/struct.MistralRs.html\" title=\"struct mistralrs_core::MistralRs\">MistralRs</a>"]]]]);
+    var implementors = Object.fromEntries([["mistralrs_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"mistralrs_core/struct.MistralRs.html\" title=\"struct mistralrs_core::MistralRs\">MistralRs</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
