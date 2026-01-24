312ea0c91b3d9c0b46b0dea0663b990f81c8ceb3
Eric Buehler <ericlbuehler@gmail.com>
2026-01-24 16:13:52 -0500

    Fix format

--- mistralrs-paged-attn/src/cuda/mod.rs
+++ mistralrs-paged-attn/src/cuda/mod.rs
@@ -4,6 +4,6 @@ mod backend;
 mod ffi;
 
 pub use backend::{
-    concat_and_cache_mla, flashinfer_mla_decode, gather_mla_cache, copy_blocks, kv_scale_update,
+    concat_and_cache_mla, copy_blocks, flashinfer_mla_decode, gather_mla_cache, kv_scale_update,
     paged_attention, reshape_and_cache, swap_blocks,
 };
