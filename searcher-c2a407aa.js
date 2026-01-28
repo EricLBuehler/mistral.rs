e249a998d00d8079293d35f4059aca432fae23c2
EricLBuehler <65165915+EricLBuehler@users.noreply.github.com>
2026-01-28 00:09:26 +0000

    Deploying to gh-pages from @ EricLBuehler/mistral.rs@d5f3be6fc638a010d80099568c1369418009546e 🚀

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-971f90c6.js',
+                path_to_root + 'searchindex-12cbfe5c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
