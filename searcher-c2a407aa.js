5b498f22d5261d7821a6c5c1a482e6d7653f8bb7
Eric Buehler <ericlbuehler@gmail.com>
2026-04-06 11:49:26 -0400

    Deploy docs

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-0499d85f.js',
+                path_to_root + 'searchindex-5b149b9f.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
